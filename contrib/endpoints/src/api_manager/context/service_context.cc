6351cfab238e0864217d2f28b4a57a5c441cc095
Sarvani Vakkalanka <vakkalanka@google.com>
2017-02-01 09:44:13 -0800

    format file

--- contrib/endpoints/src/api_manager/context/service_context.cc
+++ contrib/endpoints/src/api_manager/context/service_context.cc
@@ -63,8 +63,8 @@ ServiceContext::ServiceContext(std::unique_ptr<ApiManagerEnvInterface> env,
                                   .force_disable()),
       is_check_security_rules_disabled_(config_->server_config() &&
                                         config_->server_config()
-                                        ->api_check_security_rules_config()
-                                        .force_disable()) {
+                                            ->api_check_security_rules_config()
+                                            .force_disable()) {
   intermediate_report_interval_ = kIntermediateReportInterval;
 
   // Check server_config override.
