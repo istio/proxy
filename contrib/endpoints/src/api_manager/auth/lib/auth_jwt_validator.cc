4ef505aa2d4f153b5feb96efc1e30b7217cee6c3
Sarvani Vakkalanka <vakkalanka@google.com>
2017-02-02 13:29:54 -0800

    Address a review comment.

--- contrib/endpoints/src/api_manager/auth/lib/auth_jwt_validator.cc
+++ contrib/endpoints/src/api_manager/auth/lib/auth_jwt_validator.cc
@@ -702,8 +702,8 @@ grpc_jwt_verifier_status JwtValidatorImpl::FillUserInfoAndSetExp(
 
   char *json_str =
       grpc_json_dump_to_string(const_cast<::grpc_json *>(grpc_json), 0);
-  user_info->claims = json_str == nullptr ? "" : json_str;
   if (json_str != nullptr) {
+    user_info->claims = json_str;
     gpr_free(json_str);
   }
 
