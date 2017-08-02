72eaba66a3103c390dd8a297cd9af17c1fa34af3
mtakigiku <mtakigiku@google.com>
2017-08-02 00:01:28 +0000

    add feature to check base64url input character validity

--- src/envoy/auth/jwt.cc
+++ src/envoy/auth/jwt.cc
@@ -35,10 +35,39 @@ namespace {
 
 namespace Base64url {
 
+// Conversion table is taken from
+// https://opensource.apple.com/source/QuickTimeStreamingServer/QuickTimeStreamingServer-452/CommonUtilitiesLib/base64.c
+//
+// and modified the position of 62 ('+' to '-') and 63 ('/' to '_')
+const uint8_t REVERSE_LOOKUP_TABLE_BASE64URL[256] = {
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 64, 64, 64, 64, 64, 64, 64, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64,
+    63, 64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+    43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64};
+
+bool isNotBase64UrlChar(int8_t c) {
+  return REVERSE_LOOKUP_TABLE_BASE64URL[static_cast<int32_t>(c)] & 64;
+}
+
 std::string decode(std::string input) {
   /*
-   * TODO: check input validity
+   * if input contains non-base64url character, return empty string
    */
+  if (std::find_if(input.begin(), input.end(), isNotBase64UrlChar) !=
+      input.end()) {
+    return "";
+  }
+
   /*
    * base64url is using '-', '_' instead of '+', '/' in base64 string.
    */
@@ -59,7 +88,7 @@ std::string decode(std::string input) {
       break;
     default:
       /*
-       * for invalid base64url input, return empty string
+       * an invalid base64url input. return empty string.
        */
       return "";
   }
