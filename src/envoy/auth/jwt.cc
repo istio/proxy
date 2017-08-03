13a8b3146fd082cfef2490f9380c042757f377f8
mtakigiku <mtakigiku@google.com>
2017-08-03 21:46:57 +0000

    moved BigNumFromBase64UrlString to inside RsaFromJwk

--- src/envoy/auth/jwt.cc
+++ src/envoy/auth/jwt.cc
@@ -119,20 +119,19 @@ bssl::UniquePtr<EVP_PKEY> EvpPkeyFromStr(const std::string &pkey_pem) {
           .get());
 }
 
-BIGNUM *BigNumFromBase64UrlString(const std::string &s) {
-  std::string s_decoded = Base64UrlDecode(s);
-  if (s_decoded == "") {
-    return nullptr;
-  }
-  return BN_bin2bn(CastToUChar(s_decoded), s_decoded.length(), NULL);
-}
-
 bssl::UniquePtr<RSA> RsaFromJwk(const std::string &n, const std::string &e) {
   bssl::UniquePtr<RSA> rsa(RSA_new());
   if (!rsa) {
     // Couldn't create RSA key.
     return nullptr;
   }
+  auto BigNumFromBase64UrlString = [](const std::string &s) -> BIGNUM * {
+    std::string s_decoded = Base64UrlDecode(s);
+    if (s_decoded == "") {
+      return nullptr;
+    }
+    return BN_bin2bn(CastToUChar(s_decoded), s_decoded.length(), NULL);
+  };
   rsa->n = BigNumFromBase64UrlString(n);
   rsa->e = BigNumFromBase64UrlString(e);
   if (!rsa->n || !rsa->e) {
