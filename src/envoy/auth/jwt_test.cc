29c58fc9441112bfe60753e8f1a18a6f6fe41172
mtakigiku <mtakigiku@google.com>
2017-08-03 05:47:59 +0000

    support JWK public key

--- src/envoy/auth/jwt_test.cc
+++ src/envoy/auth/jwt_test.cc
@@ -172,6 +172,61 @@ class JwtTest : public testing::Test {
       "YjAxMGQ4MjYyYmUKM2U1MjMyMTE4MzUxY2U5M2VkNmY1NWJhYTFmNmU5M2NmMzVlZjJiNjRi"
       "MDYxNzU4YWJmYzdkNzUzYzAxMWVhNgo3NTg1N2MwMGY3YTE3Y2E3YWI2NGJlMWIyYjdkNzZl"
       "NWJlMThhZWFmZWY5NDU5MjAxY2RkY2NkZGZiZjczMjQ2";
+
+  // The following public key jwk and token are taken from
+  // https://github.com/cloudendpoints/esp/blob/master/src/api_manager/auth/lib/auth_jwt_validator_test.cc
+  const std::string kPublicKeyJwk =
+      "{\"keys\": [{\"kty\": \"RSA\",\"alg\": \"RS256\",\"use\": "
+      "\"sig\",\"kid\": \"62a93512c9ee4c7f8067b5a216dade2763d32a47\",\"n\": "
+      "\"0YWnm_eplO9BFtXszMRQNL5UtZ8HJdTH2jK7vjs4XdLkPW7YBkkm_"
+      "2xNgcaVpkW0VT2l4mU3KftR-6s3Oa5Rnz5BrWEUkCTVVolR7VYksfqIB2I_"
+      "x5yZHdOiomMTcm3DheUUCgbJRv5OKRnNqszA4xHn3tA3Ry8VO3X7BgKZYAUh9fyZTFLlkeAh"
+      "0-"
+      "bLK5zvqCmKW5QgDIXSxUTJxPjZCgfx1vmAfGqaJb-"
+      "nvmrORXQ6L284c73DUL7mnt6wj3H6tVqPKA27j56N0TB1Hfx4ja6Slr8S4EB3F1luYhATa1P"
+      "KU"
+      "SH8mYDW11HolzZmTQpRoLV8ZoHbHEaTfqX_aYahIw\",\"e\": \"AQAB\"},{\"kty\": "
+      "\"RSA\",\"alg\": \"RS256\",\"use\": \"sig\",\"kid\": "
+      "\"b3319a147514df7ee5e4bcdee51350cc890cc89e\",\"n\": "
+      "\"qDi7Tx4DhNvPQsl1ofxxc2ePQFcs-L0mXYo6TGS64CY_"
+      "2WmOtvYlcLNZjhuddZVV2X88m0MfwaSA16wE-"
+      "RiKM9hqo5EY8BPXj57CMiYAyiHuQPp1yayjMgoE1P2jvp4eqF-"
+      "BTillGJt5W5RuXti9uqfMtCQdagB8EC3MNRuU_KdeLgBy3lS3oo4LOYd-"
+      "74kRBVZbk2wnmmb7IhP9OoLc1-7-9qU1uhpDxmE6JwBau0mDSwMnYDS4G_ML17dC-"
+      "ZDtLd1i24STUw39KH0pcSdfFbL2NtEZdNeam1DDdk0iUtJSPZliUHJBI_pj8M-2Mn_"
+      "oA8jBuI8YKwBqYkZCN1I95Q\",\"e\": \"AQAB\"}]}";
+
+  // Header (kid is not specified):
+  // {
+  //   "alg": "RS256",
+  //   "typ": "JWT"
+  // }
+  // Payload:
+  // {
+  //   "iss": "628645741881-"
+  //     "noabiu23f5a8m8ovd8ucv698lj78vv0l@developer.gserviceaccount.com",
+  //   "sub": "628645741881-"
+  //     "noabiu23f5a8m8ovd8ucv698lj78vv0l@developer.gserviceaccount.com",
+  //   "aud": "http://myservice.com/myapi"
+  // }
+  const std::string kTokenNoKid =
+      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9."
+      "eyJpc3MiOiI2Mjg2NDU3NDE4ODEtbm9hYml1M"
+      "jNmNWE4bThvdmQ4dWN2Njk4bGo3OHZ2MGxAZGV2ZWxvcGVyLmdzZXJ2aWNlYWNjb3VudC5jb"
+      "20"
+      "iLCJzdWIiOiI2Mjg2NDU3NDE4ODEtbm9hYml1MjNmNWE4bThvdmQ4dWN2Njk4bGo3OHZ2MGx"
+      "AZ"
+      "GV2ZWxvcGVyLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJhdWQiOiJodHRwOi8vbXlzZXJ2aWNlL"
+      "mN"
+      "vbS9teWFwaSJ9.gq_4ucjddQDjYK5FJr_kXmMo2fgSEB6Js1zopcQLVpCKFDNb-"
+      "TQ97go0wuk5"
+      "_vlSp_8I2ImrcdwYbAKqYCzcdyBXkAYoHCGgmY-v6MwZFUvrIaDzR_"
+      "M3rmY8sQ8cdN3MN6ZRbB"
+      "6opHwDP1lUEx4bZn_ZBjJMPgqbIqGmhoT1UpfPF6P1eI7sXYru-"
+      "4KVna0STOynLl3d7JYb7E-8"
+      "ifcjUJLhat8JR4zR8i4-zWjn6d6j_"
+      "NI7ZvMROnao77D9YyhXv56zfsXRatKzzYtxPlQMz4AjP-"
+      "bUHfbHmhiIOOAeEKFuIVUAwM17j54M6VQ5jnAabY5O-ermLfwPiXvNt2L2SA==";
 };
 
 TEST_F(JwtTest, JwtDecode) {
@@ -276,6 +331,32 @@ TEST_F(JwtTest, InvalidAlg) {
   auto payload = Jwt::Decode(kJwtWithInvalidAlg, kPublicKey);
   EXPECT_FALSE(payload);
 }
+
+TEST_F(JwtTest, JwtDecodeWithJwk) {
+  auto payload = Jwt::DecodeWithJwk(kTokenNoKid, kPublicKeyJwk);
+
+  EXPECT_TRUE(payload);
+
+  EXPECT_TRUE((*payload)["iss"].IsString());
+  std::string iss = (*payload)["iss"].GetString();
+  EXPECT_STREQ(iss.c_str(),
+               "628645741881-noabiu23f5a8m8ovd8ucv698lj78vv0l@developer."
+               "gserviceaccount.com");
+
+  EXPECT_TRUE((*payload)["sub"].IsString());
+  std::string sub = (*payload)["sub"].GetString();
+  EXPECT_STREQ(sub.c_str(),
+               "628645741881-noabiu23f5a8m8ovd8ucv698lj78vv0l@developer."
+               "gserviceaccount.com");
+
+  EXPECT_TRUE((*payload)["aud"].IsString());
+  std::string aud = (*payload)["aud"].GetString();
+  EXPECT_STREQ(aud.c_str(), "http://myservice.com/myapi");
+}
+
+/*
+ * TODO: add failure tests for JWKs
+ */
 
 }  // namespace Auth
 }  // namespace Http
