b35635b0ba8f2e3945f8a301ae2b81b7747ae472
Costin Manolache <costin@google.com>
2017-08-16 13:49:18 -0700

    Fix scripts

--- tools/deb/istio-iptables.sh
+++ tools/deb/istio-iptables.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Initialization script responsible for setting up port forwarding for Istio sidecar.
+# Based on pilot/docker/prepare_proxy.sh - but instead of capturing all traffic, only capture
+# configured ranges.
 
 set -o nounset
 set -o pipefail
 IFS=,
 
 ISTIO_SIDECAR_CONFIG=${ISTIO_SIDECAR_CONFIG:-/var/lib/istio/envoy/sidecar.env}
-if [ ! -r $ISTIO_SIDECAR_CONFIG ]; then
-  echo "Missing sidecar config: $ISTIO_SIDECAR_CONFIG"
-  exit 1
+if [ -r ${ISTIO_SIDECAR_CONFIG} ]; then
+  . ${ISTIO_SIDECAR_CONFIG}
 fi
 
-. $ISTIO_SIDECAR_CONFIG
-
+# TODO: more flexibility - maybe a whitelist of users to be captured for output instead of
+# a blacklist.
 if [ -z "${ENVOY_UID:-}" ]; then
-  # Default to the UID of ENVOY_USER
+  # Default to the UID of ENVOY_USER and root
   ENVOY_UID=$(id -u ${ENVOY_USER:-istio})
   if [ ! $? == 0 ]; then
      echo "Invalid istio user $ENVOY_UID $ENVOY_USER"
@@ -23,65 +24,78 @@ if [ -z "${ENVOY_UID:-}" ]; then
   ENVOY_UID=${ENVOY_UID},0
 fi
 
+# Remove the old chains, to generate new configs.
+iptables -t nat -D PREROUTING -p tcp -j ISTIO_INBOUND 2>/dev/null
+iptables -t nat -D OUTPUT -p tcp -j ISTIO_OUTPUT 2>/dev/null
+
+# Flush and delete the istio chains
+iptables -t nat -F ISTIO_OUTPUT 2>/dev/null
+iptables -t nat -X ISTIO_OUTPUT 2>/dev/null
+iptables -t nat -F ISTIO_INBOUND 2>/dev/null
+iptables -t nat -X ISTIO_INBOUND 2>/dev/null
+iptables -t nat -F ISTIO_REDIRECT 2>/dev/null
+iptables -t nat -X ISTIO_REDIRECT 2>/dev/null
+
 if [ "${1:-}" = "clean" ]; then
-  iptables -F -t nat
-  iptables -t nat -X ISTIO_REDIRECT
-  iptables -t nat -X ISTIO_OUTPUT
+  # Only cleanup, don't add new rules.
   exit 0
 fi
 
-# Remove the chains, to generate new configs.
-iptables -t nat -X ISTIO_REDIRECT
-iptables -t nat -X ISTIO_OUTPUT
-
 # Create a new chain for redirecting inbound traffic to the common Envoy port.
-iptables -t nat -N ISTIO_REDIRECT                                             -m comment --comment "istio/redirect-common-chain"
-iptables -t nat -A ISTIO_REDIRECT -p tcp -j REDIRECT --to-port ${ENVOY_PORT:-15001}  -m comment --comment "istio/redirect-to-envoy-port"
+# In the ISTIO_INBOUND and ISTIO_OUTBOUND chains, '-j RETURN' bypasses Envoy
+# and '-j ISTIO_REDIRECT' redirects to Envoy.
+iptables -t nat -N ISTIO_REDIRECT
+iptables -t nat -A ISTIO_REDIRECT -p tcp -j REDIRECT --to-port ${ENVOY_PORT:-15001}
 
-# Makes sure SSH is not redirectred
-iptables -t nat -A PREROUTING -p tcp --dport 22 -j RETURN
+# Handling of inbound ports. Traffic will be redirected to Envoy, which will process and forward
+# to the local service.
+if [ -n "${ISTIO_INBOUND_PORTS:-}" ]; then
+  iptables -t nat -N ISTIO_INBOUND
+  iptables -t nat -A PREROUTING -p tcp -j ISTIO_INBOUND
 
-if [ "${ISTIO_LOCAL_PORTS:-}" == "*" ]; then
-     for port in ${ISTIO_LOCAL_EXCLUDE_PORTS}; do
-        iptables -t nat -A PREROUTING -p tcp --dport ${port} -j RETURN
-     done
-     iptables -t nat -A PREROUTING -p tcp -j ISTIO_REDIRECT
-elif [ ! -z "${ISTIO_LOCAL_PORTS:-}" ]; then
-    for port in ${ISTIO_LOCAL_PORTS}; do
-        iptables -t nat -A PREROUTING -p tcp --dport ${port} -j ISTIO_REDIRECT
-    done
+  # Makes sure SSH is not redirectred
+  iptables -t nat -A ISTIO_INBOUND -p tcp --dport 22 -j RETURN
+
+  if [ "${ISTIO_INBOUND_PORTS:-}" == "*" ]; then
+       for port in ${ISTIO_LOCAL_EXCLUDE_PORTS:-}; do
+          iptables -t nat -A ISTIO_INBOUND -p tcp --dport ${port} -j RETURN
+       done
+       iptables -t nat -A ISTIO_INBOUND -p tcp -j ISTIO_REDIRECT
+  else
+      for port in ${ISTIO_INBOUND_PORTS}; do
+          iptables -t nat -A ISTIO_INBOUND -p tcp --dport ${port} -j ISTIO_REDIRECT
+      done
+  fi
 fi
 
 # Create a new chain for selectively redirecting outbound packets to Envoy.
-iptables -t nat -N ISTIO_OUTPUT                                               -m comment --comment "istio/common-output-chain"
+iptables -t nat -N ISTIO_OUTPUT
 
-# Jump to the ISTIO_OUTPUT chain from OUTPUT chain for all tcp
-# traffic. '-j RETURN' bypasses Envoy and '-j ISTIO_REDIRECT'
-# redirects to Envoy.
-iptables -t nat -A OUTPUT -p tcp -j ISTIO_OUTPUT                              -m comment --comment "istio/install-istio-output"
+# Jump to the ISTIO_OUTPUT chain from OUTPUT chain for all tcp traffic.
+iptables -t nat -A OUTPUT -p tcp -j ISTIO_OUTPUT
 
 # Redirect app calls to back itself via Envoy when using the service VIP or endpoint
 # address, e.g. appN => Envoy (client) => Envoy (server) => appN.
-iptables -t nat -A ISTIO_OUTPUT -o lo ! -d 127.0.0.1/32 -j ISTIO_REDIRECT     -m comment --comment "istio/redirect-implicit-loopback"
+iptables -t nat -A ISTIO_OUTPUT -o lo ! -d 127.0.0.1/32 -j ISTIO_REDIRECT
 
 for uid in ${ENVOY_UID}; do
   # Avoid infinite loops. Don't redirect Envoy traffic directly back to
   # Envoy for non-loopback traffic.
-  iptables -t nat -A ISTIO_OUTPUT -m owner --uid-owner ${uid} -j RETURN   -m comment --comment "istio/bypass-envoy"
+  iptables -t nat -A ISTIO_OUTPUT -m owner --uid-owner ${uid} -j RETURN
 done
 
 # Skip redirection for Envoy-aware applications and
 # container-to-container traffic both of which explicitly use
 # localhost.
-iptables -t nat -A ISTIO_OUTPUT -d 127.0.0.1/32 -j RETURN                     -m comment --comment "istio/bypass-explicit-loopback"
+iptables -t nat -A ISTIO_OUTPUT -d 127.0.0.1/32 -j RETURN
 
 IFS=,
-if [ "${ISTIO_SERVICE_CIDR}" != "" ]; then
+if [ -n "${ISTIO_SERVICE_CIDR:-}" ]; then
     for cidr in ${ISTIO_SERVICE_CIDR}; do
-        iptables -t nat -A ISTIO_OUTPUT -d ${cidr} -j ISTIO_REDIRECT          -m comment --comment "istio/redirect-ip-range-${cidr}"
+        iptables -t nat -A ISTIO_OUTPUT -d ${cidr} -j ISTIO_REDIRECT
     done
-    iptables -t nat -A ISTIO_OUTPUT -j RETURN                                 -m comment --comment "istio/bypass-default-outbound"
+    iptables -t nat -A ISTIO_OUTPUT -j RETURN
 else
-    iptables -t nat -A ISTIO_OUTPUT -j ISTIO_REDIRECT                         -m comment --comment "istio/redirect-default-outbound"
+    iptables -t nat -A ISTIO_OUTPUT -j ISTIO_REDIRECT
 fi
 
--- tools/deb/istio-start.sh
+++ tools/deb/istio-start.sh
@@ -7,24 +7,26 @@ set -e
 
 ISTIO_SIDECAR_CONFIG=${ISTIO_SIDECAR_CONFIG:-/var/lib/istio/envoy/sidecar.env}
 
-if [[! -f $ISTIO_SIDECAR_CONFIG]]; then
-  echo "Missing sidecar config: $ISTIO_SIDECAR_CONFIG"
-  exit 1
+if [[ -r ${ISTIO_SIDECAR_CONFIG} ]]; then
+  . $ISTIO_SIDECAR_CONFIG
 fi
 
-. $ISTIO_SIDECAR_CONFIG
+ISTIO_BIN_BASE=${ISTIO_BIN_BASE:-/usr/local/bin}
+ISTIO_LOG_DIR=${ISTIO_LOG_DIR:-/var/log/istio}
+ISTIO_CFG=${ISTIO_CFG:-/var/lib/istio}
 
-/usr/local/bin/istio-iptables.sh
+# Update iptables
+${ISTIO_BIN_BASE}/istio-iptables.sh
 
 
-if [ -f /usr/local/bin/pilot-agent ]; then
-  exec su - istio /usr/local/bin/pilot-agent proxy > /var/log/istio/istio.log 2>&1
+if [ -f ${ISTIO_BIN_BASE}/pilot-agent ]; then
+  exec su - istio ${ISTIO_BIN_BASE}/pilot-agent proxy > ${ISTIO_LOG_DIR}/istio.log 2>&1
 else
-  if [ "" = "$SVC_IP" ]; then
-    SVC_IP=$(hostname --ip-address)
+  if [ -z "${ISTIO_SVC_IP:-}" ]; then
+    ISTIO_SVC_IP=$(hostname --ip-address)
   fi
   # Run envoy directly - agent not installed. This should be used only for debugging/testing standalone envoy
-  exec su - istio -c "/usr/local/bin/envoy -c /var/lib/istio/envoy/envoy.json --restart-epoch 0 --drain-time-s 2 --parent-shutdown-time-s 3 --service-cluster istio-proxy --service-node '$SVC_IP|mysvc.default|cluster.local' $ISTIO_DEBUG"
+  exec su - istio -c "${ISTIO_BIN_BASE}/envoy -c ${ISTIO_CFG}/envoy/envoy.json --restart-epoch 0 --drain-time-s 2 --parent-shutdown-time-s 3 --service-cluster istio-proxy --service-node 'sidecar~${ISTIO_SVC_IP}~mysvc.${ISTIO_NAMESPACE:-default}~cluster.local' $ISTIO_DEBUG"
 fi
 
 
