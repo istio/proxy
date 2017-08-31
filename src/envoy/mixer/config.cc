4e31265e82cc9acfeb7705617e4501fb9a919c74
Wayne Zhang <qiwzhang@google.com>
2017-08-31 18:35:07 +0000

    add test.

--- src/envoy/mixer/config.cc
+++ src/envoy/mixer/config.cc
@@ -33,9 +33,10 @@ const std::string kForwardAttributes("forward_attributes");
 const std::string kQuotaName("quota_name");
 const std::string kQuotaAmount("quota_amount");
 
-// The Json object name to disable check cache and quota cache
+// The Json object name to disable check cache, quota cache and report batch
 const std::string kDisableCheckCache("disable_check_cache");
 const std::string kDisableQuotaCache("disable_quota_cache");
+const std::string kDisableReportBatch("disable_report_batch");
 
 const std::string kNetworkFailPolicy("network_fail_policy");
 const std::string kDisableTcpCheckCalls("disable_tcp_check_calls");
@@ -45,6 +46,14 @@ void ReadString(const Json::Object& json, const std::string& name,
   if (json.hasObject(name)) {
     *value = json.getString(name);
   }
+}
+
+bool ReadBoolean(const Json::Object& json, const std::string& name,
+                 bool defaultValue) {
+  if (json.hasObject(name)) {
+    return json.getBoolean(name, defaultValue);
+  }
+  return defaultValue;
 }
 
 void ReadStringMap(const Json::Object& json, const std::string& name,
@@ -69,9 +78,11 @@ void MixerConfig::Load(const Json::Object& json) {
 
   ReadString(json, kNetworkFailPolicy, &network_fail_policy);
 
-  ReadString(json, kDisableCheckCache, &disable_check_cache);
-  ReadString(json, kDisableQuotaCache, &disable_quota_cache);
-  disable_tcp_check_calls = json.getBoolean(kDisableTcpCheckCalls, false);
+  disable_check_cache = ReadBoolean(json, kDisableCheckCache, false);
+  disable_quota_cache = ReadBoolean(json, kDisableQuotaCache, false);
+  disable_report_batch = ReadBoolean(json, kDisableReportBatch, false);
+
+  disable_tcp_check_calls = ReadBoolean(json, kDisableTcpCheckCalls, false);
 }
 
 void MixerConfig::ExtractQuotaAttributes(Attributes* attr) const {
