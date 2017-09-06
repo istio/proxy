a5869eac6adc1b5e50dc84ca51ac770ac9c2a80f
Costin Manolache <costin@google.com>
2017-09-06 11:23:48 -0700

    Test fixes

--- tools/deb/test/istio_common.sh
+++ tools/deb/test/istio_common.sh
@@ -49,6 +49,8 @@ function istio_build_all() {
 
 }
 
+
+
 # Build docker images for istio. Intended to test backward compat.
 function istio_build_docker() {
   local TAG=${1:-$(whoami)}
@@ -72,6 +74,17 @@ function istio_k8s_update() {
   for y in *.yaml; do
     kubectl apply -f $y
   done
+}
+
+# Helper to get the external IP of a raw VM
+function istioVMExternalIP() {
+  local NAME=${1:-$TESTVM}
+  gcloud compute --project $PROJECT instances describe $NAME --zone $ISTIO_ZONE --format='value(networkInterfaces[0].accessConfigs[0].natIP)'
+}
+
+function istioVMInternalIP() {
+  local NAME=${1:-$TESTVM}
+  gcloud compute --project $PROJECT instances describe $NAME  --zone $ISTIO_ZONE --format='value(networkInterfaces[0].networkIP)'
 }
 
 
@@ -107,3 +120,146 @@ function istioVMCopy() {
 
   gcloud compute scp  --zone $ISTIO_ZONE --recurse $FILES ${NAME}:
 }
+
+# Helper to use the raw istio config templates directly.
+function istioTmpl() {
+  local FILE=$1
+
+  local SED="s,{PROXY_HUB},$HUB,; s,{PROXY_TAG},$TAG,; "
+  SED="$SED s,{MIXER_HUB},$HUB,; s,{MIXER_TAG},$TAG,; "
+  SED="$SED s,{PILOT_HUB},$HUB,; s,{PILOT_TAG},$TAG,; "
+  SED="$SED s,{CA_HUB},$HUB,; s,{CA_TAG},$TAG,; "
+
+  sed  "$SED" $1
+
+}
+# Install Istio components. The test environment may be already setup, this function is needed to quickly setup
+# the components for the rawVM testing in a new cluster.
+function istioInstallCluster() {
+    pushd $GOPATH/src/istio.io/istio/install/kubernetes/templates
+    istioTmpl istio-ingress.yaml | kubectl apply -f -
+    istioTmpl istio-mixer.yaml | kubectl apply -f -
+    istioTmpl istio-pilot.yaml | kubectl apply -f -
+    cd istio-auth
+    istioTmpl istio-namespace-ca.yaml | kubectl apply -f -
+
+    cd ../../addons
+    kubectl apply -f grafana.yaml
+    kubectl apply -f prometheus.yaml
+    kubectl apply -f zipkin.yaml
+
+    ISTIOCTL=$GOPATH/src/istio.io/pilot/bazel-bin/cmd/istioctl/istioctl
+
+    cd ../../../samples/apps/bookinfo
+    kubectl apply -f <($ISTIOCTL kube-inject --hub $HUB --tag $TAG -f bookinfo.yaml)
+
+    popd
+}
+
+# Initialize the K8S cluster, generating config files for the raw VMs.
+# Must be run once, will generate files in the CWD. The files must be installed on the VM.
+# This assumes the recommended dnsmasq config option.
+function istioPrepareCluster() {
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: istio-pilot-ilb
+  annotations:
+    cloud.google.com/load-balancer-type: "internal"
+  labels:
+    istio: pilot
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 8080
+    protocol: TCP
+  selector:
+    istio: pilot
+EOF
+cat <<EOF | kubectl apply -n kube-system -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: dns-ilb
+  annotations:
+    cloud.google.com/load-balancer-type: "internal"
+  labels:
+    k8s-app: kube-dns
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 53
+    protocol: UDP
+  selector:
+    k8s-app: kube-dns
+EOF
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: mixer-ilb
+  annotations:
+    cloud.google.com/load-balancer-type: "internal"
+  labels:
+    istio: mixer
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 9091
+    protocol: TCP
+  selector:
+    istio: mixer
+EOF
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: istio-ca-ilb
+  annotations:
+    cloud.google.com/load-balancer-type: "internal"
+  labels:
+    istio: istio-ca
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 8060
+    protocol: TCP
+  selector:
+    istio: istio-ca
+EOF
+
+  for i in {1..10}
+  do
+    PILOT_IP=$(kubectl get service istio-pilot-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    ISTIO_DNS=$(kubectl get -n kube-system service dns-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    MIXER_IP=$(kubectl get service mixer-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    CA_IP=$(kubectl get service istio-ca-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+    if [ ${PILOT_IP} == "" -o  ${PILOT_IP} == "" -o ${MIXER_IP} == "" ] ; then
+        echo Waiting for ILBs
+        sleep 10
+    else
+        break
+    fi
+  done
+
+  if [ ${PILOT_IP} == "" -o  ${PILOT_IP} == "" -o ${MIXER_IP} == "" ] ; then
+    echo "Failed to create ILBs"
+    exit 1
+  fi
+
+  #/etc/dnsmasq.d/kubedns
+  echo "server=/default.svc.cluster.local/$ISTIO_DNS" > kubedns
+  echo "address=/istio-mixer/$MIXER_IP" >> kubedns
+  echo "address=/mixer-server/$MIXER_IP" >> kubedns
+  echo "address=/istio-pilot/$PILOT_IP" >> kubedns
+  echo "address=/istio-ca/$CA_IP" >> kubedns
+
+  CIDR=$(gcloud container clusters describe ${K8SCLUSTER} --project ${PROJECT} --zone=${ISTIO_ZONE} --format "value(servicesIpv4Cidr)")
+  echo "ISTIO_SERVICE_CIDR=$CIDR" > cluster.env
+
+}
+
--- tools/deb/test/run_gce_test.sh
+++ tools/deb/test/run_gce_test.sh
@@ -36,11 +36,11 @@ PROXY_DIR=${ISTIO_IO}/proxy
 TAG=${TAG:-$(whoami)}
 HUB=${ISTIO_HUB:-gcr.io/istio-testing}
 
+. $PROXY_DIR/tools/deb/test/istio_common.sh
 
 # TODO: extend the script to use Vagrant+minikube or other ways to create the VM. The main
 # issue is that we need the k8s and VM to be on same VPC - unless we run kubeapiserver+etcd
 # standalone.
-set -x
 
 # Run a command in a VM.
 function istioRun() {
@@ -116,158 +116,6 @@ function istioVMDelete() {
   gcloud compute -q  instances delete $NAME --zone $ISTIO_ZONE --project $PROJECT
 }
 
-# Helper to get the external IP of a raw VM
-function istioVMExternalIP() {
-  local NAME=${1:-$TESTVM}
-  gcloud compute --project $PROJECT instances describe $NAME --zone $ISTIO_ZONE --format='value(networkInterfaces[0].accessConfigs[0].natIP)'
-}
-
-function istioVMInternalIP() {
-  local NAME=${1:-$TESTVM}
-  gcloud compute --project $PROJECT instances describe $NAME  --zone $ISTIO_ZONE --format='value(networkInterfaces[0].networkIP)'
-}
-
-# Helper to use the raw istio config templates directly.
-function istioTmpl() {
-  local FILE=$1
-
-  local SED="s,{PROXY_HUB},$HUB,; s,{PROXY_TAG},$TAG,; "
-  SED="$SED s,{MIXER_HUB},$HUB,; s,{MIXER_TAG},$TAG,; "
-  SED="$SED s,{PILOT_HUB},$HUB,; s,{PILOT_TAG},$TAG,; "
-  SED="$SED s,{CA_HUB},$HUB,; s,{CA_TAG},$TAG,; "
-
-  sed  "$SED" $1
-
-}
-# Install Istio components. The test environment may be already setup, this function is needed to quickly setup
-# the components for the rawVM testing in a new cluster.
-function istioInstallCluster() {
-    pushd $GOPATH/src/istio.io/istio/install/kubernetes/templates
-    istioTmpl istio-ingress.yaml | kubectl apply -f -
-    istioTmpl istio-mixer.yaml | kubectl apply -f -
-    istioTmpl istio-pilot.yaml | kubectl apply -f -
-    cd istio-auth
-    istioTmpl istio-namespace-ca.yaml | kubectl apply -f -
-
-    cd ../../addons
-    kubectl apply -f grafana.yaml
-    kubectl apply -f prometheus.yaml
-    kubectl apply -f zipkin.yaml
-
-    ISTIOCTL=$GOPATH/src/istio.io/pilot/bazel-bin/cmd/istioctl/istioctl
-
-    cd ../../../samples/apps/bookinfo
-    kubectl apply -f <($ISTIOCTL kube-inject --hub $HUB --tag $TAG -f bookinfo.yaml)
-
-    popd
-}
-
-# Initialize the K8S cluster, generating config files for the raw VMs.
-# Must be run once, will generate files in the CWD. The files must be installed on the VM.
-# This assumes the recommended dnsmasq config option.
-function istioPrepareCluster() {
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: istio-pilot-ilb
-  annotations:
-    cloud.google.com/load-balancer-type: "internal"
-  labels:
-    istio: pilot
-spec:
-  type: LoadBalancer
-  ports:
-  - port: 8080
-    protocol: TCP
-  selector:
-    istio: pilot
-EOF
-cat <<EOF | kubectl apply -n kube-system -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: dns-ilb
-  annotations:
-    cloud.google.com/load-balancer-type: "internal"
-  labels:
-    k8s-app: kube-dns
-spec:
-  type: LoadBalancer
-  ports:
-  - port: 53
-    protocol: UDP
-  selector:
-    k8s-app: kube-dns
-EOF
-
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: mixer-ilb
-  annotations:
-    cloud.google.com/load-balancer-type: "internal"
-  labels:
-    istio: mixer
-spec:
-  type: LoadBalancer
-  ports:
-  - port: 9091
-    protocol: TCP
-  selector:
-    istio: mixer
-EOF
-
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: istio-ca-ilb
-  annotations:
-    cloud.google.com/load-balancer-type: "internal"
-  labels:
-    istio: istio-ca
-spec:
-  type: LoadBalancer
-  ports:
-  - port: 8060
-    protocol: TCP
-  selector:
-    istio: istio-ca
-EOF
-
-  for i in {1..10}
-  do
-    PILOT_IP=$(kubectl get service istio-pilot-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    ISTIO_DNS=$(kubectl get -n kube-system service dns-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    MIXER_IP=$(kubectl get service mixer-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    CA_IP=$(kubectl get service istio-ca-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-
-    if [ ${PILOT_IP} == "" -o  ${PILOT_IP} == "" -o ${MIXER_IP} == "" ] ; then
-        echo Waiting for ILBs
-        sleep 10
-    else
-        break
-    fi
-  done
-
-  if [ ${PILOT_IP} == "" -o  ${PILOT_IP} == "" -o ${MIXER_IP} == "" ] ; then
-    echo "Failed to create ILBs"
-    exit 1
-  fi
-
-  #/etc/dnsmasq.d/kubedns
-  echo "server=/default.svc.cluster.local/$ISTIO_DNS" > kubedns
-  echo "address=/istio-mixer/$MIXER_IP" >> kubedns
-  echo "address=/mixer-server/$MIXER_IP" >> kubedns
-  echo "address=/istio-pilot/$PILOT_IP" >> kubedns
-  echo "address=/istio-ca/$CA_IP" >> kubedns
-
-  CIDR=$(gcloud container clusters describe ${K8SCLUSTER} --project ${PROJECT} --zone=${ISTIO_ZONE} --format "value(servicesIpv4Cidr)")
-  echo "ISTIO_SERVICE_CIDR=$CIDR" > cluster.env
-
-}
 
 # Configure a service running on the VM.
 # Params:
@@ -442,12 +290,12 @@ function ingressIP() {
   kubectl get service istio-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
 }
 
-function setUp() {
+function istioTestSetUp() {
 
  LOCAL_IP=$(istioVMInternalIP $TESTVM)
 
  # Configure a service for the local nginx server, and add an ingress route
- istioConfigHttpService $TESTVM 80 $LOCAL_IP
+ istioConfigService $TESTVM 80 $LOCAL_IP
 
  # Bookinfo components on the VM
  istioConfigService ${TESTVM}-details 9080 $LOCAL_IP
@@ -479,7 +327,7 @@ function testRawVMToCluster() {
   echo $?
 }
 
-function test() {
+function istioTest() {
   testRawVMToCluster
   testClusterToRawVM
 }
@@ -494,8 +342,8 @@ if [[ ${1:-} == "init" ]] ; then
 elif [[ ${1:-} == "env" ]] ; then
   echo "test environment variables enabled"
 elif [[ ${1:-} == "test" ]] ; then
-  setUp
-  test
+  istioTestSetUp
+  istioTest
 elif [[ ${1:-} == "help" ]] ; then
   echo "$0 init: provision an existing VM using the current build"
   echo "$0 test: run tests"
@@ -506,7 +354,7 @@ else
   tools/deb/test/build_all.sh
   istioVMInit ${TESTVM}
   istioProvisionTestWorker ${TESTVM}
-  setUp
-  test
+  istioTestSetUp
+  istioTest
 fi
 
