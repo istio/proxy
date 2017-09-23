7e7b418b1b9481123b147cdb8cc5f366538d420b
Sebastien Vas <sebastienvas@google.com>
2017-09-22 17:45:25 -0700

    Remove kms calls as we don't publish docker images

--- script/cloud_builder.sh
+++ script/cloud_builder.sh
@@ -5,25 +5,7 @@ set -o nounset
 set -o pipefail
 set -x
 
-# Cloud Builder does not support running as a user
-# Note the image used has a releng user, but we start as root
-# and need to su into releng
-
-RELENG_HOME='/home/releng'
-WORKSPACE="${PWD}"
-
-echo 'Downloading docker.io encoded config'
-gsutil cp gs://istio-secrets/dockerhub_config.json.enc \
-  "${RELENG_HOME}/config.json.enc"
-
-echo 'Decoding docker.io config'
-gcloud kms decrypt \
-  --ciphertext-file="${RELENG_HOME}/config.json.enc" \
-  --plaintext-file="${RELENG_HOME}/config.json" \
-  --location=global \
-  --keyring=Secrets \
-  --key=DockerHub \
-  --verbosity=info
+# Use this file for Cloud Builder specific settings.
 
 echo 'Setting bazel.rc'
 cp tools/bazel.rc.cloudbuilder "${HOME}/.bazelrc"
