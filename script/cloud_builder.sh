e0a6b2b64c9f6a99bfcc86052a36f61d61623018
Matt Delco <delco@google.com>
2017-11-06 14:58:06 -0800

    initial CB changes

--- script/cloud_builder.sh
+++ script/cloud_builder.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
+# Copyright 2017 Istio Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
 
 set -o errexit
 set -o nounset
 set -o pipefail
 set -x
 
-# Use this file for Cloud Builder specific settings.
+OUTPUT_PATH=""
+TAG_NAME="0.0.0"
 
+function usage() {
+  echo "$0
+    -o        path to store build artifacts
+    -t <tag>  tag to use (optional, defaults to ${TAG_NAME} )"
+  exit 1
+}
+
+while getopts o:t: arg ; do
+  case "${arg}" in
+    o) OUTPUT_PATH="${OPTARG}";;
+    t) TAG_NAME="${OPTARG}";;
+    *) usage;;
+  esac
+done
+
+[[ -z "${OUTPUT_PATH}" ]] && usage
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd $ROOT
+
+# Use this file for Cloud Builder specific settings.
 echo 'Setting bazel.rc'
 cp tools/bazel.rc.cloudbuilder "${HOME}/.bazelrc"
 
-script/release.sh ${@}
+./script/push-debian.sh -c opt -v "${TAG_NAME}" -d "${OUTPUT_PATH}"
