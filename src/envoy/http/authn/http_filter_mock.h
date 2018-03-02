638eef86b0032a1b181ad451136c5f45a9e26cb5
Diem Vu <diemvu@google.com>
2018-03-01 19:21:33 -0800

    Reformat http_filter_mock

--- src/envoy/http/authn/http_filter_mock.h
+++ src/envoy/http/authn/http_filter_mock.h
@@ -32,18 +32,14 @@ class MockAuthenticationFilter : public AuthenticationFilter {
       : AuthenticationFilter(proto_config) {}
   ~MockAuthenticationFilter(){};
 
-  MOCK_CONST_METHOD3(
-      ValidateX509,
-      void(const HeaderMap& headers,
-           const iaapi::MutualTls& params,
-           const AuthenticateDoneCallback& done_callback));
-  MOCK_CONST_METHOD3(ValidateJwt,
+  MOCK_CONST_METHOD3(ValidateX509,
                      void(const HeaderMap& headers,
-                          const iaapi::Jwt& params,
+                          const iaapi::MutualTls& params,
                           const AuthenticateDoneCallback& done_callback));
-  const IstioAuthN::Context& context() {
-    return context_;
-  }
+  MOCK_CONST_METHOD3(ValidateJwt,
+                     void(const HeaderMap& headers, const iaapi::Jwt& params,
+                          const AuthenticateDoneCallback& done_callback));
+  const IstioAuthN::Context& context() { return context_; }
 };
 
 }  // namespace Http
