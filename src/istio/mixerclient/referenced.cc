33ca4fb79fffe473a9b6436a3d5a6121f7a28441
Sky Ao <aoxiaojian@gmail.com>
2018-04-27 15:33:31 +0800

    refactory code

--- src/istio/mixerclient/referenced.cc
+++ src/istio/mixerclient/referenced.cc
@@ -117,8 +117,21 @@ bool Referenced::Fill(const Attributes &attributes,
 bool Referenced::Signature(const Attributes &attributes,
                            const std::string &extra_key,
                            std::string *signature) const {
-  const auto &attributes_map = attributes.attributes();
+  if (!checkIfAbsenceKeysMatched(attributes, extra_key)) {
+    return false;
+  }
 
+  if (!checkIfExactKeysMatched(attributes, extra_key)) {
+    return false;
+  }
+
+  doSignature(attributes, extra_key, signature);
+  return true;
+}
+
+bool Referenced::checkIfAbsenceKeysMatched(const Attributes &attributes,
+                                           const std::string &extra_key) const {
+  const auto &attributes_map = attributes.attributes();
   for (std::size_t i = 0; i < absence_keys_.size(); ++i) {
     const auto &key = absence_keys_[i];
     const auto it = attributes_map.find(key.name);
@@ -152,6 +165,12 @@ bool Referenced::Signature(const Attributes &attributes,
 
     } while (true);
   }
+  return true;
+}
+
+bool Referenced::checkIfExactKeysMatched(const Attributes &attributes,
+                                         const std::string &extra_key) const {
+  const auto &attributes_map = attributes.attributes();
 
   // First loop for exact_keys
   // DON‘T DO Signature in this loop!
@@ -165,28 +184,34 @@ bool Referenced::Signature(const Attributes &attributes,
 
     const Attributes_AttributeValue &value = it->second;
     if (value.value_case() == Attributes_AttributeValue::kStringMapValue) {
-        std::string map_key = key.map_key;
-        const auto &smap = value.string_map_value().entries();
-        // Since exact_keys_ are sorted by key.name,
-        // continue processing stringMaps until a new name is found.
-        do {
-          const auto sub_it = smap.find(map_key);
-          // exact match of map_key is missing
-          if (sub_it == smap.end()) {
-            return false;
-          }
+      std::string map_key = key.map_key;
+      const auto &smap = value.string_map_value().entries();
+      // Since exact_keys_ are sorted by key.name,
+      // continue processing stringMaps until a new name is found.
+      do {
+        const auto sub_it = smap.find(map_key);
+        // exact match of map_key is missing
+        if (sub_it == smap.end()) {
+          return false;
+        }
 
-          // break loop if at the end or keyname changes.
-          if (i + 1 == exact_keys_.size() ||
-              exact_keys_[i + 1].name != key.name) {
-            break;
-          }
+        // break loop if at the end or keyname changes.
+        if (i + 1 == exact_keys_.size() ||
+            exact_keys_[i + 1].name != key.name) {
+          break;
+        }
 
-          map_key = exact_keys_[++i].map_key;
-        } while (true);
+        map_key = exact_keys_[++i].map_key;
+      } while (true);
     }
   }
+  return true;
+}
 
+void Referenced::doSignature(const Attributes &attributes,
+                             const std::string &extra_key,
+                             std::string *signature) const {
+  const auto &attributes_map = attributes.attributes();
 
   utils::MD5 hasher;
 
@@ -263,7 +288,6 @@ bool Referenced::Signature(const Attributes &attributes,
   hasher.Update(extra_key);
 
   *signature = hasher.Digest();
-  return true;
 }
 
 std::string Referenced::Hash() const {
