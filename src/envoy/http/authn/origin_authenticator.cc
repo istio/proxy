72610f632f5aecd23f8dd5b4b743bd52ac45782a
Yangmin Zhu <ymzhu@google.com>
2018-09-11 11:48:34 -0700

    update

--- src/envoy/http/authn/origin_authenticator.cc
+++ src/envoy/http/authn/origin_authenticator.cc
@@ -34,42 +34,40 @@ bool OriginAuthenticator::run(Payload* payload) {
   bool triggered_success = false;
   bool triggered = false;
 
-  if (policy_.origins_size() == 0) {
-    if (policy_.principal_binding() == iaapi::PrincipalBinding::USE_ORIGIN) {
-      // Validation should reject policy that have rule to USE_ORIGIN but
-      // does not provide any origin method so this code should
-      // never reach. However, it's ok to treat it as authentication
-      // fails.
-      ENVOY_LOG(warn,
-                "Principal is binded to origin, but no method specified in "
-                "policy {}",
-                policy_.DebugString());
-      return false;
-    }
+  if (policy_.origins_size() == 0 &&
+      policy_.principal_binding() == iaapi::PrincipalBinding::USE_ORIGIN) {
+    // Validation should reject policy that have rule to USE_ORIGIN but
+    // does not provide any origin method so this code should
+    // never reach. However, it's ok to treat it as authentication
+    // fails.
+    ENVOY_LOG(warn,
+              "Principal is binded to origin, but no method specified in "
+              "policy {}",
+              policy_.DebugString());
+    return false;
+  }
+  const char* request_path = nullptr;
+  if (filter_context()->headerMap().Path() != nullptr) {
+    request_path = filter_context()->headerMap().Path()->value().c_str();
+    ENVOY_LOG(debug, "Got request path {}", request_path);
   } else {
-    const char* request_path = nullptr;
-    if (filter_context()->headerMap().Path() != nullptr) {
-      request_path = filter_context()->headerMap().Path()->value().c_str();
-      ENVOY_LOG(debug, "Got request path {}", request_path);
-    } else {
-      ENVOY_LOG(error,
-                "Failed to get request path, JWT will always be used for "
-                "validation");
-    }
+    ENVOY_LOG(error,
+              "Failed to get request path, JWT will always be used for "
+              "validation");
+  }
 
-    for (const auto& method : policy_.origins()) {
-      const auto& jwt = method.jwt();
+  for (const auto& method : policy_.origins()) {
+    const auto& jwt = method.jwt();
 
-      if (AuthnUtils::ShouldValidateJwtPerPath(request_path, jwt)) {
-        ENVOY_LOG(debug, "Validating request path {} for jwt {}", request_path,
-                  jwt.DebugString());
-        // set triggered to true if any of the jwt trigger rule matched.
-        triggered = true;
-        if (validateJwt(jwt, payload)) {
-          ENVOY_LOG(debug, "JWT validation succeeded");
-          triggered_success = true;
-          break;
-        }
+    if (AuthnUtils::ShouldValidateJwtPerPath(request_path, jwt)) {
+      ENVOY_LOG(debug, "Validating request path {} for jwt {}", request_path,
+                jwt.DebugString());
+      // set triggered to true if any of the jwt trigger rule matched.
+      triggered = true;
+      if (validateJwt(jwt, payload)) {
+        ENVOY_LOG(debug, "JWT validation succeeded");
+        triggered_success = true;
+        break;
       }
     }
   }
@@ -78,8 +76,11 @@ bool OriginAuthenticator::run(Payload* payload) {
   if (!triggered || triggered_success) {
     filter_context()->setOriginResult(payload);
     filter_context()->setPrincipal(policy_.principal_binding());
+    ENVOY_LOG(debug, "Origin authenticator succeeded");
     return true;
   }
+
+  ENVOY_LOG(debug, "Origin authenticator failed");
   return false;
 }
 
