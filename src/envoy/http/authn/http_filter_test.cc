634dd620ebedfd1ecd32f6c8433daa40706caacf
Jimmy Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-09-17 17:07:24 -0700

    Update Proxy SHA (#1978)
    Signed-off-by: JimmyCYJ <jimmychen.0102@gmail.com>

--- src/envoy/http/authn/http_filter_test.cc
+++ src/envoy/http/authn/http_filter_test.cc
@@ -24,6 +24,7 @@
 #include "src/envoy/http/authn/test_utils.h"
 #include "src/envoy/utils/authn.h"
 #include "test/mocks/http/mocks.h"
+#include "test/test_common/test_time.h"
 #include "test/test_common/utility.h"
 
 using Envoy::Http::Istio::AuthN::AuthenticatorBase;
@@ -120,7 +121,9 @@ TEST_F(AuthenticationFilterTest, PeerFail) {
   EXPECT_CALL(filter_, createPeerAuthenticator(_))
       .Times(1)
       .WillOnce(Invoke(createAlwaysFailAuthenticator));
-  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2);
+  DangerousDeprecatedTestTime test_time;
+  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2,
+                                            test_time.timeSystem());
   EXPECT_CALL(decoder_callbacks_, requestInfo())
       .Times(AtLeast(1))
       .WillRepeatedly(ReturnRef(request_info));
@@ -144,7 +147,9 @@ TEST_F(AuthenticationFilterTest, PeerPassOriginFail) {
   EXPECT_CALL(filter_, createOriginAuthenticator(_))
       .Times(1)
       .WillOnce(Invoke(createAlwaysFailAuthenticator));
-  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2);
+  DangerousDeprecatedTestTime test_time;
+  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2,
+                                            test_time.timeSystem());
   EXPECT_CALL(decoder_callbacks_, requestInfo())
       .Times(AtLeast(1))
       .WillRepeatedly(ReturnRef(request_info));
@@ -166,7 +171,9 @@ TEST_F(AuthenticationFilterTest, AllPass) {
   EXPECT_CALL(filter_, createOriginAuthenticator(_))
       .Times(1)
       .WillOnce(Invoke(createAlwaysPassAuthenticator));
-  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2);
+  DangerousDeprecatedTestTime test_time;
+  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2,
+                                            test_time.timeSystem());
   EXPECT_CALL(decoder_callbacks_, requestInfo())
       .Times(AtLeast(1))
       .WillRepeatedly(ReturnRef(request_info));
@@ -235,7 +242,9 @@ TEST_F(AuthenticationFilterTest, IgnoreBothPass) {
   EXPECT_CALL(filter, createOriginAuthenticator(_))
       .Times(1)
       .WillOnce(Invoke(createAlwaysPassAuthenticator));
-  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2);
+  DangerousDeprecatedTestTime test_time;
+  RequestInfo::RequestInfoImpl request_info(Http::Protocol::Http2,
+                                            test_time.timeSystem());
   EXPECT_CALL(decoder_callbacks_, requestInfo())
       .Times(AtLeast(1))
       .WillRepeatedly(ReturnRef(request_info));
