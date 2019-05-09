69ceed5ebaec9b02fa6fdda73e1c30e57cdf0c94
Hai Huang <huang195@gmail.com>
2019-05-09 00:34:56 -0400

    fix test

--- test/integration/int_server.cc
+++ test/integration/int_server.cc
@@ -27,7 +27,7 @@
 #include "common/network/raw_buffer_socket.h"
 #include "envoy/http/codec.h"
 #include "envoy/network/transport_socket.h"
-#include "test/test_common/thread_factory_for_test.h"
+#include "envoy/test/test_common/thread_factory_for_test.h"
 #include "fmt/printf.h"
 #include "server/connection_handler_impl.h"
 #include "test/test_common/network_utility.h"
