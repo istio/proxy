ec539a2e9e73bf354b27516f7b0c686bd8f385ec
Hai Huang <huang195@gmail.com>
2019-05-16 16:19:16 -0400

    remove ctags file

--- include/istio/mixerclient/options.h
+++ include/istio/mixerclient/options.h
@@ -52,13 +52,13 @@ struct CheckOptions {
   uint32_t max_retry_ms{1000};
 };
 
-// Default to batch up to 100 reports or 1000 milliseconds (1 second).
 const int DEFAULT_BATCH_REPORT_MAX_ENTRIES = 100;
 const int DEFAULT_BATCH_REPORT_MAX_TIME_MS = 1000;
 
 // Options controlling report batch.
 struct ReportOptions {
   // Default constructor.
+  // Default to batch up to 100 reports or 1000 milliseconds (1 second).
   ReportOptions()
       : max_batch_entries(DEFAULT_BATCH_REPORT_MAX_ENTRIES),
         max_batch_time_ms(DEFAULT_BATCH_REPORT_MAX_TIME_MS) {}
