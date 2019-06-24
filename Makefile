istio/proxy

commit ac78dc0193284534bea9e9551f784ee6d33494b2

Import common files into this repo. (#2251)

Makefile
@@ -53,16 +53,18 @@ test_asan:
 test_tsan:
 	PATH=$(PATH) CC=$(CC) CXX=$(CXX) bazel $(BAZEL_STARTUP_ARGS) test $(BAZEL_TEST_ARGS) --config=clang-tsan --test_env=TSAN_OPTIONS=suppressions=$(TOP)/tsan.suppressions -- $(BAZEL_TARGETS) $(SANITIZER_EXCLUSIONS)
 
-check:
-	@script/check-license-headers
-	@script/check-repositories
-	@script/check-style
+lint:
+	@scripts/check_licenses.sh
+	@scripts/check-repositories.sh
+	@scripts/check-style.sh
 
 artifacts: build
-	@script/push-debian.sh -c opt -p $(ARTIFACTS_DIR)
+	@scripts/push-debian.sh -c opt -p $(ARTIFACTS_DIR)
 
 deb:
 	CC=$(CC) CXX=$(CXX) bazel $(BAZEL_STARTUP_ARGS) build $(BAZEL_BUILD_ARGS) //tools/deb:istio-proxy
 
 
 .PHONY: build clean test check artifacts
+
+include Makefile.common.mk

.commonfiles.sha
@@ -1,0 +1,1 @@
+5747f694803650e785957bd4e716604b6c8310aa

.golangci.yml
@@ -1,0 +1,188 @@
+# The original version of this file is located in the https://github.com/istio/common-files repo.
+# If you're looking at this file in a different repo and want to make a change, please go to the
+# common-files repo, make the change there and check it in. Then come back to this repo and run the
+# scripts/updatecommonfiles.sh script.
+
+service:
+  # When updating this, also update bin/linters.sh accordingly
+  golangci-lint-version: 1.16.x # use the fixed version to not introduce new linters unexpectedly
+run:
+  # timeout for analysis, e.g. 30s, 5m, default is 1m
+  deadline: 20m
+
+  # which dirs to skip: they won't be analyzed;
+  # can use regexp here: generated.*, regexp is applied on full path;
+  # default value is empty list, but next dirs are always skipped independently
+  # from this option's value:
+  #   	vendor$, third_party$, testdata$, examples$, Godeps$, builtin$
+  skip-dirs:
+    - genfiles$
+    - vendor$
+
+  # which files to skip: they will be analyzed, but issues from them
+  # won't be reported. Default value is empty list, but there is
+  # no need to include all autogenerated files, we confidently recognize
+  # autogenerated files. If it's not please let us know.
+  skip-files:
+    - ".*\\.pb\\.go"
+    - ".*\\.gen\\.go"
+
+linters:
+  enable-all: true
+  disable:
+    - depguard
+    - dupl
+    - gochecknoglobals
+    - gochecknoinits
+    - goconst
+    - gocyclo
+    - gosec
+    - nakedret
+    - prealloc
+    - scopelint
+  fast: false
+
+linters-settings:
+  errcheck:
+    # report about not checking of errors in type assetions: `a := b.(MyStruct)`;
+    # default is false: such cases aren't reported by default.
+    check-type-assertions: false
+
+    # report about assignment of errors to blank identifier: `num, _ := strconv.Atoi(numStr)`;
+    # default is false: such cases aren't reported by default.
+    check-blank: false
+  govet:
+    # report about shadowed variables
+    check-shadowing: false
+  golint:
+    # minimal confidence for issues, default is 0.8
+    min-confidence: 0.0
+  gofmt:
+    # simplify code: gofmt with `-s` option, true by default
+    simplify: true
+  goimports:
+    # put imports beginning with prefix after 3rd-party packages;
+    # it's a comma-separated list of prefixes
+    local-prefixes: istio.io/
+  maligned:
+    # print struct with more effective memory layout or not, false by default
+    suggest-new: true
+  misspell:
+    # Correct spellings using locale preferences for US or UK.
+    # Default is to use a neutral variety of English.
+    # Setting locale to US will correct the British spelling of 'colour' to 'color'.
+    locale: US
+  lll:
+    # max line length, lines longer will be reported. Default is 120.
+    # '\t' is counted as 1 character by default, and can be changed with the tab-width option
+    line-length: 160
+    # tab width in spaces. Default to 1.
+    tab-width: 1
+  unused:
+    # treat code as a program (not a library) and report unused exported identifiers; default is false.
+    # XXX: if you enable this setting, unused will report a lot of false-positives in text editors:
+    # if it's called for subdir of a project it can't find funcs usages. All text editor integrations
+    # with golangci-lint call it on a directory with the changed file.
+    check-exported: false
+  unparam:
+    # call graph construction algorithm (cha, rta). In general, use cha for libraries,
+    # and rta for programs with main packages. Default is cha.
+    algo: cha
+
+    # Inspect exported functions, default is false. Set to true if no external program/library imports your code.
+    # XXX: if you enable this setting, unparam will report a lot of false-positives in text editors:
+    # if it's called for subdir of a project it can't find external interfaces. All text editor integrations
+    # with golangci-lint call it on a directory with the changed file.
+    check-exported: false
+  gocritic:
+    enabled-checks:
+      - appendCombine
+      - argOrder
+      - assignOp
+      - badCond
+      - boolExprSimplify
+      - builtinShadow
+      - captLocal
+      - caseOrder
+      - codegenComment
+      - commentedOutCode
+      - commentedOutImport
+      - defaultCaseOrder
+      - deprecatedComment
+      - docStub
+      - dupArg
+      - dupBranchBody
+      - dupCase
+      - dupSubExpr
+      - elseif
+      - emptyFallthrough
+      - equalFold
+      - flagDeref
+      - flagName
+      - hexLiteral
+      - indexAlloc
+      - initClause
+      - methodExprCall
+      - nilValReturn
+      - octalLiteral
+      - offBy1
+      - rangeExprCopy
+      - regexpMust
+      - sloppyLen
+      - stringXbytes
+      - switchTrue
+      - typeAssertChain
+      - typeSwitchVar
+      - typeUnparen
+      - underef
+      - unlambda
+      - unnecessaryBlock
+      - unslice
+      - valSwap
+      - weakCond
+      - yodaStyleExpr
+
+      # Unused
+      # - appendAssign
+      # - commentFormatting
+      # - emptyStringTest
+      # - exitAfterDefer
+      # - ifElseChain
+      # - hugeParam
+      # - importShadow
+      # - nestingReduce
+      # - paramTypeCombine
+      # - ptrToRefParam
+      # - rangeValCopy
+      # - singleCaseSwitch
+      # - sloppyReassign
+      # - unlabelStmt
+      # - unnamedResult
+      # - wrapperFunc
+
+issues:
+  # List of regexps of issue texts to exclude, empty list by default.
+  # But independently from this option we use default exclude patterns,
+  # it can be disabled by `exclude-use-default: false`. To list all
+  # excluded by default patterns execute `golangci-lint run --help`
+  exclude:
+    - composite literal uses unkeyed fields
+
+  exclude-rules:
+    # Exclude some linters from running on test files.
+    - path: _test\.go$|^tests/|^samples/
+      linters:
+        - errcheck
+        - maligned
+
+  # Independently from option `exclude` we use default exclude patterns,
+  # it can be disabled by this option. To list all
+  # excluded by default patterns execute `golangci-lint run --help`.
+  # Default value for this option is true.
+  exclude-use-default: true
+
+  # Maximum issues count per one linter. Set to 0 to disable. Default is 50.
+  max-per-linter: 0
+
+  # Maximum count of issues with the same text. Set to 0 to disable. Default is 3.
+  max-same-issues: 0

LICENSE
@@ -187,7 +187,7 @@
       same "printed page" as the copyright notice for easier
       identification within third-party archives.
 
-   Copyright 2016 Istio Authors
+   Copyright 2019 Istio Authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.

Makefile.common.mk
@@ -1,0 +1,27 @@
+# WARNING: DO NOT EDIT, THIS FILE IS PROBABLY A COPY
+#
+# The original version of this file is located in the https://github.com/istio/common-files repo.
+# If you're looking at this file in a different repo and want to make a change, please go to the
+# common-files repo, make the change there and check it in. Then come back to this repo and run the
+# scripts/updatecommonfiles.sh script.
+
+# Copyright 2018 Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+updatecommon:
+	@git clone https://github.com/istio/common-files
+	@cd common-files
+	@git rev-parse HEAD >.commonfiles.sha
+	@cp -r common-files/files/* common-files/files/.[^.]* .
+	@rm -fr common-files

prow/proxy-postsubmit.sh
@@ -45,5 +45,5 @@ GIT_SHA="$(git rev-parse --verify HEAD)"
 cd $ROOT
 
 echo 'Create and push artifacts'
-script/release-binary
+scripts/release-binary.sh
 ARTIFACTS_DIR="gs://istio-artifacts/proxy/${GIT_SHA}/artifacts/debs" make artifacts

script/check-license-headers
@@ -1,57 +1,0 @@
-#!/bin/bash
-#
-# Copyright 2017 Istio Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-################################################################################
-#
-# Checks the source code files for proper license headers.
-
-ISTIO_COPYRIGHT='Copyright 20[0-9][0-9] Istio Authors\. All Rights Reserved\.'
-ISTIO_LICENSE='Apache License, Version 2\.0'
-
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
-function is_source_code_file() {
-  first_line="$(head -1 ${1} | xargs --null)"
-  filename="$(basename ${1})"
-  [[ "${filename}" == 'BUILD' ]] && return 0
-  [[ "${first_line}" == '#!/bin/bash' ]] && return 0
-  extension="$(echo ${1} | awk -F . '{if (NF>1) {print $NF}}')"
-  [[ "${extension}" == "c" ||
-     "${extension}" == "cc" ||
-     "${extension}" == "h" ||
-     "${extension}" == "py" ||
-     "${extension}" == "go" ||
-     "${extension}" == "bzl" ]] && return 0
-  return 1
-}
-
-BAD_LICENSE=0
-
-for file in $(git ls-files)
-do
-  base="$(echo ${file} | awk -F / '{print $1}')"
-  [[ "${base}" == "contrib" || "${base}" == "google" || "${base}" == "third_party" ]] && continue
-  if is_source_code_file "${file}"; then
-    istio_copyright_count="$(head -n 15 ${file} | grep "${ISTIO_COPYRIGHT}" | wc -l)"
-    istio_license_count="$(head -n 15 ${file} | grep "${ISTIO_LICENSE}" | wc -l)"
-    if [[ "${istio_copyright_count}" != 1 || "${istio_license_count}" != 1 ]]; then
-      echo ${file}
-      BAD_LICENSE=1
-    fi
-  fi
-done
-
-[[ ${BAD_LICENSE} == 0 ]] || (echo "Found invalid license headers." && exit 1)

commit 45c0224518c55cfb6cd37990918d3b4335d050b4

Fix checks on master.
Broken in #2251.

Signed-off-by: Piotr Sikora <piotrsikora@google.com>

.circleci/config.yml
@@ -16,7 +16,7 @@ jobs:
       # To build docker containers or run tests in a docker
       - setup_remote_docker
       - run: rm ~/.gitconfig
-      - run: make check
+      - run: make lint
       - run: make deb
       - run: make test
       - save_cache:

Makefile
@@ -53,9 +53,13 @@ test_asan:
 test_tsan:
 	PATH=$(PATH) CC=$(CC) CXX=$(CXX) bazel $(BAZEL_STARTUP_ARGS) test $(BAZEL_TEST_ARGS) --config=clang-tsan --test_env=TSAN_OPTIONS=suppressions=$(TOP)/tsan.suppressions -- $(BAZEL_TARGETS) $(SANITIZER_EXCLUSIONS)
 
+check:
+	@echo >&2 "Please use \"make lint\" instead."
+	@false
+
 lint:
-	@scripts/check_licenses.sh
-	@scripts/check-repositories.sh
+	@scripts/check_license.sh
+	@scripts/check-repository.sh
 	@scripts/check-style.sh
 
 artifacts: build

prow/proxy-presubmit.sh
@@ -51,7 +51,7 @@ cd $ROOT
 export BAZEL_TEST_ARGS="--test_output=errors"
 
 echo 'Code Check'
-make check
+make lint
 
 echo 'Bazel Build'
 make build

src/envoy/http/authn/filter_context.cc
@@ -74,17 +74,17 @@ void FilterContext::setPrincipal(const iaapi::PrincipalBinding& binding) {
 
 bool FilterContext::getJwtPayload(const std::string& issuer,
                                   std::string* payload) const {
-  // Prefer to use the jwt payload from Envoy jwt filter over the Istio jwt filter's one.
+  // Prefer to use the jwt payload from Envoy jwt filter over the Istio jwt
+  // filter's one.
   return getJwtPayloadFromEnvoyJwtFilter(issuer, payload) ||
          getJwtPayloadFromIstioJwtFilter(issuer, payload);
 }
 
-bool FilterContext::getJwtPayloadFromEnvoyJwtFilter(const std::string& issuer,
-                                  std::string* payload) const {
+bool FilterContext::getJwtPayloadFromEnvoyJwtFilter(
+    const std::string& issuer, std::string* payload) const {
   // Try getting the Jwt payload from Envoy jwt_authn filter.
-  auto filter_it =
-      dynamic_metadata_.filter_metadata().find(
-        Extensions::HttpFilters::HttpFilterNames::get().JwtAuthn);
+  auto filter_it = dynamic_metadata_.filter_metadata().find(
+      Extensions::HttpFilters::HttpFilterNames::get().JwtAuthn);
   if (filter_it == dynamic_metadata_.filter_metadata().end()) {
     ENVOY_LOG(debug, "No dynamic_metadata found for filter {}",
               Extensions::HttpFilters::HttpFilterNames::get().JwtAuthn);
@@ -102,15 +102,17 @@ bool FilterContext::getJwtPayloadFromEnvoyJwtFilter(const std::string& issuer,
     return false;
   }
 
-  // Serialize the payload from Envoy jwt filter first before writing it to |payload|.
-  // TODO (pitlv2109): Return protobuf Struct instead of string, once Istio jwt filter is removed.
-  // Also need to change how Istio authn filter processes the jwt payload.
+  // Serialize the payload from Envoy jwt filter first before writing it to
+  // |payload|.
+  // TODO (pitlv2109): Return protobuf Struct instead of string, once Istio jwt
+  // filter is removed. Also need to change how Istio authn filter processes the
+  // jwt payload.
   Protobuf::util::MessageToJsonString(entry_it->second.struct_value(), payload);
   return true;
 }
 
-bool FilterContext::getJwtPayloadFromIstioJwtFilter(const std::string& issuer,
-                                  std::string* payload) const {
+bool FilterContext::getJwtPayloadFromIstioJwtFilter(
+    const std::string& issuer, std::string* payload) const {
   // Try getting the Jwt payload from Istio jwt-auth filter.
   auto filter_it =
       dynamic_metadata_.filter_metadata().find(Utils::IstioFilterName::kJwt);

src/envoy/http/authn/filter_context.h
@@ -21,8 +21,8 @@
 #include "envoy/config/filter/http/authn/v2alpha1/config.pb.h"
 #include "envoy/http/filter.h"
 #include "envoy/network/connection.h"
-#include "src/istio/authn/context.pb.h"
 #include "extensions/filters/http/well_known_names.h"
+#include "src/istio/authn/context.pb.h"
 
 namespace Envoy {
 namespace Http {
@@ -76,12 +76,14 @@ class FilterContext : public Logger::Loggable<Logger::Id::filter> {
   const HeaderMap& headerMap() const { return header_map_; }
 
  private:
-  // Helper function for getJwtPayload(). It gets the jwt payload from Envoy jwt filter metadata
-  // and write to |payload|.
-  bool getJwtPayloadFromEnvoyJwtFilter(const std::string& issuer, std::string* payload) const;
-  // Helper function for getJwtPayload(). It gets the jwt payload from Istio jwt filter metadata
-  // and write to |payload|.
-  bool getJwtPayloadFromIstioJwtFilter(const std::string& issuer, std::string* payload) const;
+  // Helper function for getJwtPayload(). It gets the jwt payload from Envoy jwt
+  // filter metadata and write to |payload|.
+  bool getJwtPayloadFromEnvoyJwtFilter(const std::string& issuer,
+                                       std::string* payload) const;
+  // Helper function for getJwtPayload(). It gets the jwt payload from Istio jwt
+  // filter metadata and write to |payload|.
+  bool getJwtPayloadFromIstioJwtFilter(const std::string& issuer,
+                                       std::string* payload) const;
 
   // Const reference to request info dynamic metadata. This provides data that
   // output from other filters, e.g JWT.

test/integration/istio_http_integration_test_with_envoy_jwt_filter.cc
@@ -19,6 +19,7 @@
 // metadata) and that information can only be observed at the end (i.e from
 // request to mixer backends).
 
+#include "extensions/filters/http/well_known_names.h"
 #include "fmt/printf.h"
 #include "gmock/gmock.h"
 #include "include/istio/utils/attribute_names.h"
@@ -26,7 +27,6 @@
 #include "src/envoy/utils/filter_names.h"
 #include "src/envoy/utils/trace_headers.h"
 #include "test/integration/http_protocol_integration.h"
-#include "extensions/filters/http/well_known_names.h"
 
 using ::google::protobuf::util::error::Code;
 using ::testing::Contains;
@@ -163,7 +163,8 @@ std::string MakeEnvoyJwtFilterConfig() {
       "4WTiULmmHSGZHOjzwa8WtrtOQGsAFjIbno85jp6MnGGGZPYZbDAa_b3y5u-"
       "YpW7ypZrvD8BgtKVjgtQgZhLAGezMt0ua3DRrWnKqTZ0BJ_EyxOGuHJrLsn00fnMQ\"}]}";
 
-  return fmt::sprintf(kJwtFilterTemplate, Extensions::HttpFilters::HttpFilterNames::get().JwtAuthn,
+  return fmt::sprintf(kJwtFilterTemplate,
+                      Extensions::HttpFilters::HttpFilterNames::get().JwtAuthn,
                       StringUtil::escape(kJwksInline),
                       StringUtil::escape(kJwksInline));
 }
@@ -234,32 +235,34 @@ std::string MakeMixerFilterConfig() {
                       kPolicyBackend);
 }
 
-
-// checkJwtRawClaim finds the serialized jwt payload and check to see if all key-value pairs
-// from the jwt claim is there.
-// Returns false if it cannot find the serialized jwt payload or if any key-value pair does not
-// matched the expectation.
-bool checkJwtRawClaim(const ::google::protobuf::RepeatedPtrField<::std::string>& words) {
+// checkJwtRawClaim finds the serialized jwt payload and check to see if all
+// key-value pairs from the jwt claim is there. Returns false if it cannot find
+// the serialized jwt payload or if any key-value pair does not matched the
+// expectation.
+bool checkJwtRawClaim(
+    const ::google::protobuf::RepeatedPtrField<::std::string>& words) {
   for (auto& word : words) {
-    google::protobuf::Struct payload; 
-    Protobuf::util::Status status =  Protobuf::util::JsonStringToMessage(word.data(), &payload);
+    google::protobuf::Struct payload;
+    Protobuf::util::Status status =
+        Protobuf::util::JsonStringToMessage(word.data(), &payload);
     if (status.ok()) {
-      if ((word.find(kExpectedIss) == std::string::npos) || 
-          (word.find(kExpectedIat) == std::string::npos) || 
-          (word.find(kExpectedExp) == std::string::npos) || 
-          (word.find(kExpectedSub) == std::string::npos) || 
+      if ((word.find(kExpectedIss) == std::string::npos) ||
+          (word.find(kExpectedIat) == std::string::npos) ||
+          (word.find(kExpectedExp) == std::string::npos) ||
+          (word.find(kExpectedSub) == std::string::npos) ||
           (word.find(kExpectedFoo) == std::string::npos)) {
-        return false; 
+        return false;
       }
       return true;
     }
   }
-  return false; 
+  return false;
 }
 
 // This integration is exact the same as one in istio_http_integration_test.cc,
 // except this test uses Envoy jwt filter, rather than Istio jwt filter.
-class IstioHttpIntegrationTestWithEnvoyJwtFilter : public HttpProtocolIntegrationTest {
+class IstioHttpIntegrationTestWithEnvoyJwtFilter
+    : public HttpProtocolIntegrationTest {
  public:
   void createUpstreams() override {
     HttpProtocolIntegrationTest::createUpstreams();
@@ -488,12 +491,12 @@ TEST_P(IstioHttpIntegrationTestWithEnvoyJwtFilter, GoodJwt) {
   ::istio::mixer::v1::CheckRequest check_request;
   waitForPolicyRequest(&check_request);
   // Check request should see authn attributes.
-  EXPECT_THAT(check_request.attributes().words(),
-              ::testing::AllOf(
-                  Contains(kDestinationUID), Contains("10.0.0.1"),
-                  Contains(kExpectedPrincipal),
-                  Contains("testing@secure.istio.io"), Contains("sub"),
-                  Contains("iss"), Contains("foo"), Contains("bar")));
+  EXPECT_THAT(
+      check_request.attributes().words(),
+      ::testing::AllOf(Contains(kDestinationUID), Contains("10.0.0.1"),
+                       Contains(kExpectedPrincipal),
+                       Contains("testing@secure.istio.io"), Contains("sub"),
+                       Contains("iss"), Contains("foo"), Contains("bar")));
   EXPECT_TRUE(checkJwtRawClaim(check_request.attributes().words()));
   sendPolicyResponse();
 
@@ -507,12 +510,12 @@ TEST_P(IstioHttpIntegrationTestWithEnvoyJwtFilter, GoodJwt) {
   ::istio::mixer::v1::ReportRequest report_request;
   waitForTelemetryRequest(&report_request);
   // Report request should also see the same authn attributes.
-  EXPECT_THAT(report_request.default_words(),
-              ::testing::AllOf(
-                  Contains(kDestinationUID), Contains("10.0.0.1"),
-                  Contains("testing@secure.istio.io"), Contains("sub"),
-                  Contains("iss"), Contains("foo"), Contains("bar")));
-	EXPECT_TRUE(checkJwtRawClaim(check_request.attributes().words()));
+  EXPECT_THAT(
+      report_request.default_words(),
+      ::testing::AllOf(Contains(kDestinationUID), Contains("10.0.0.1"),
+                       Contains("testing@secure.istio.io"), Contains("sub"),
+                       Contains("iss"), Contains("foo"), Contains("bar")));
+  EXPECT_TRUE(checkJwtRawClaim(check_request.attributes().words()));
   sendTelemetryResponse();
 
   EXPECT_TRUE(response->complete());