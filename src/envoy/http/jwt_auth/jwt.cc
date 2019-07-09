c85e798c42c39b2698fd5675bdd7a0a9de1cd423
Daniel Grimm <dgn@users.noreply.github.com>
2019-07-09 23:11:13 +0200

    Add support for RS384 and RS512 algorithms in JWT filter (#15380) (#2301)
    * Add tests for RS384 and RS512 tokens
    
    * Implement RS384/RS512 token verification
    
    * Fix the formatting

--- src/envoy/http/jwt_auth/jwt.cc
+++ src/envoy/http/jwt_auth/jwt.cc
@@ -273,7 +273,8 @@ Jwt::Jwt(const std::string &jwt) {
     return;
   }
 
-  if (alg_ != "RS256" && alg_ != "ES256") {
+  if (alg_ != "RS256" && alg_ != "ES256" && alg_ != "RS384" &&
+      alg_ != "RS512") {
     UpdateStatus(Status::ALG_NOT_IMPLEMENTED);
     return;
   }
@@ -409,14 +410,23 @@ bool Verifier::Verify(const Jwt &jwt, const Pubkeys &pubkeys) {
         VerifySignatureEC(pubkey->ec_key_.get(), jwt.signature_, signed_data)) {
       // Verification succeeded.
       return true;
-    } else if ((pubkey->pem_format_ || pubkey->kty_ == "RSA") &&
-               VerifySignatureRSA(pubkey->evp_pkey_.get(), EVP_sha256(),
-                                  jwt.signature_, signed_data)) {
-      // Verification succeeded.
-      return true;
+    } else if (pubkey->pem_format_ || pubkey->kty_ == "RSA") {
+      const EVP_MD *md;
+      if (jwt.alg_ == "RS384") {
+        md = EVP_sha384();
+      } else if (jwt.alg_ == "RS512") {
+        md = EVP_sha512();
+      } else {
+        // default to SHA256
+        md = EVP_sha256();
+      }
+      if (VerifySignatureRSA(pubkey->evp_pkey_.get(), md, jwt.signature_,
+                             signed_data)) {
+        // Verification succeeded.
+        return true;
+      }
     }
   }
-
   // Verification failed.
   if (kid_alg_matched) {
     UpdateStatus(Status::JWT_INVALID_SIGNATURE);
