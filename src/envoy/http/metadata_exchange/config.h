5b1ed95616d090a5140e3be4efe6fdaa12bcd091
Kuat Yessenov <kuat@google.com>
2019-07-16 11:21:57 -0700

    simplify
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- src/envoy/http/metadata_exchange/config.h
+++ src/envoy/http/metadata_exchange/config.h
@@ -1,3 +1,18 @@
+/* Copyright 2019 Istio Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include "extensions/common/wasm/null/null.h"
@@ -7,8 +22,6 @@ namespace Extensions {
 namespace Wasm {
 namespace MetadataExchange {
 
-// Node ID is expected to be a primary key for the node metadata
-constexpr char ExchangeIDHeader[] = "x-envoy-peer-id";
 constexpr char ExchangeMetadataHeader[] = "x-envoy-peer-metadata";
 
 // NodeMetadata key is the key in the node metadata struct that is passed
@@ -56,8 +69,6 @@ class PluginContext : public Context {
   Http::FilterHeadersStatus onResponseHeaders() override;
 
  private:
-  // pre-populated serialized values for passing to peers
-  std::string id_value_;
   std::string metadata_value_;
 };
 
