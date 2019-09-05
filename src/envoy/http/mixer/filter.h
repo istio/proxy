18b58d918ed4a8fdb6f39a54529f1a6a7e0b1dcb
Pengyuan Bian <bianpengyuan@google.com>
2019-09-05 09:34:47 -0700

    address comment

--- src/envoy/http/mixer/filter.h
+++ src/envoy/http/mixer/filter.h
@@ -77,7 +77,7 @@ class Filter : public StreamFilter,
  private:
   // Read per-route config.
   void ReadPerRouteConfig(
-      const PerRouteServiceConfig* route_cfg,
+      const PerRouteServiceConfig& route_cfg,
       ::istio::control::http::Controller::PerRouteConfig* config);
 
   // Update header maps
