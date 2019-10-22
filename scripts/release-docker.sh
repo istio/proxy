9231f5cdbc1d4e2dc43bec09dc5268430ef115a2
Kuat Yessenov <kuat@google.com>
2019-10-21 21:30:15 -0700

    ci: add docker push postsubmit
    Signed-off-by: Kuat Yessenov <kuat@google.com>

---
+++ scripts/release-docker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Copyright 2016 Istio Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+#
+# Requires:
+# - linux environment
+# - docker authorization
+#
+set -e
+set -x
+
+# Use clang for the release builds.
+export PATH=/usr/lib/llvm-9/bin:$PATH
+export CC=${CC:-clang}
+export CXX=${CXX:-clang++}
+
+# Docker tag
+export GIT_SHA="$(git rev-parse --verify HEAD)"
+export TAG="${TAG:-${GIT_SHA}}"
+
+# Add --config=libc++ if wasn't passed already.
+if [[ "${BAZEL_BUILD_ARGS}" != *"--config=libc++"* ]]; then
+  BAZEL_BUILD_ARGS="${BAZEL_BUILD_ARGS} --config=libc++"
+fi
+
+for config in release release-test dbg
+do
+  case $config in
+    "release" )
+      PARAMS="--config=release";;
+    "release-test")
+      PARAMS="--config=clang-asan --config=release-symbol";;
+    "dbg")
+      PARAMS="-c dbg"
+  esac
+  bazel run ${BAZEL_BUILD_ARGS} ${PARAMS} --define tag=${config}-${TAG} //tools/docker:push_envoy
+done
