bfdcf529ad7c21df1245c0a768af92ad13bbac1a
Douglas Reid <douglas-reid@users.noreply.github.com>
2019-11-05 15:30:54 -0800

    fix(edges): add debug log for generated requests
    Signed-off-by: Douglas Reid <douglas-reid@users.noreply.github.com>

--- extensions/stackdriver/edges/mesh_edges_service_client.cc
+++ extensions/stackdriver/edges/mesh_edges_service_client.cc
@@ -87,6 +87,9 @@ MeshEdgesServiceClientImpl::MeshEdgesServiceClientImpl(
 
 void MeshEdgesServiceClientImpl::reportTrafficAssertions(
     const ReportTrafficAssertionsRequest& request) const {
+
+  logDebug("mesh edge services client: sending request '" + request.DebugString() + "'");
+
   context_->grpcSimpleCall(
       grpc_service_, kMeshEdgesService, kReportTrafficAssertions, request,
       kDefaultTimeoutMillisecond, success_callback_, failure_callback_);
