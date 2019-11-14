6b734d5407a459a0b91829b40c8c87473d7992aa
Yangmin Zhu <ymzhu@google.com>
2019-11-13 18:27:06 -0800

    fix lint

--- src/envoy/http/authn/http_filter_integration_test.cc
+++ src/envoy/http/authn/http_filter_integration_test.cc
@@ -169,8 +169,8 @@ TEST_P(AuthenticationFilterIntegrationTest, CORSPreflight) {
   config_helper_.addFilter(kAuthnFilterWithJwt);
   initialize();
 
-  // The AuthN filter requires JWT but should bypass CORS preflight request even it doesn't have
-  // JWT token.
+  // The AuthN filter requires JWT but should bypass CORS preflight request even
+  // it doesn't have JWT token.
   codec_client_ =
       makeHttpConnection(makeClientConnection((lookupPort("http"))));
   auto headers = Http::TestHeaderMapImpl{
