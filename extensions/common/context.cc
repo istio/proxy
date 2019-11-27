40b548be12aacc9f95357725f6e6400fea0d1f5e
Douglas Reid <douglas-reid@noreply.github.com>
2019-11-27 18:04:36 +0000

    undo bad format

--- extensions/common/context.cc
+++ extensions/common/context.cc
@@ -24,7 +24,7 @@
 #ifndef NULL_PLUGIN
 #include "proxy_wasm_intrinsics.h"
 
-#else // NULL_PLUGIN
+#else  // NULL_PLUGIN
 
 #include "absl/strings/str_split.h"
 #include "extensions/common/wasm/null/null_plugin.h"
@@ -37,67 +37,56 @@ using Envoy::Extensions::Common::Wasm::Null::Plugin::getStringValue;
 using Envoy::Extensions::Common::Wasm::Null::Plugin::getStructValue;
 using Envoy::Extensions::Common::Wasm::Null::Plugin::getValue;
 
-#endif // NULL_PLUGIN
+#endif  // NULL_PLUGIN
 
 // END WASM_PROLOG
 
-namespace Wasm
-{
-namespace Common
-{
+namespace Wasm {
+namespace Common {
 
 const char kRbacFilterName[] = "envoy.filters.http.rbac";
 const char kRbacPermissivePolicyIDField[] = "shadow_effective_policy_id";
 const char kRbacPermissiveEngineResultField[] = "shadow_engine_result";
 
-namespace
-{
+namespace {
 
 // Extract fqdn from Istio cluster name, e.g.
 // inbound|9080|http|productpage.default.svc.cluster.local. If cluster name does
 // not follow Istio convention, fqdn will be left as empty string.
-void extractFqdn(const std::string &cluster_name, std::string *fqdn)
-{
+void extractFqdn(const std::string &cluster_name, std::string *fqdn) {
   const std::vector<std::string> &parts = absl::StrSplit(cluster_name, '|');
-  if (parts.size() == 4)
-  {
+  if (parts.size() == 4) {
     *fqdn = parts[3];
   }
 }
 
 // Extract service name from service fqdn.
-void extractServiceName(const std::string &fqdn, std::string *service_name)
-{
+void extractServiceName(const std::string &fqdn, std::string *service_name) {
   const std::vector<std::string> &parts = absl::StrSplit(fqdn, '.');
-  if (parts.size() > 0)
-  {
+  if (parts.size() > 0) {
     *service_name = parts[0];
   }
 }
 
-} // namespace
+}  // namespace
 
-StringView AuthenticationPolicyString(ServiceAuthenticationPolicy policy)
-{
-  switch (policy)
-  {
-  case ServiceAuthenticationPolicy::None:
-    return kNone;
-  case ServiceAuthenticationPolicy::MutualTLS:
-    return kMutualTLS;
-  default:
-    break;
+StringView AuthenticationPolicyString(ServiceAuthenticationPolicy policy) {
+  switch (policy) {
+    case ServiceAuthenticationPolicy::None:
+      return kNone;
+    case ServiceAuthenticationPolicy::MutualTLS:
+      return kMutualTLS;
+    default:
+      break;
   }
   return {};
   ;
 }
 
 // Retrieves the traffic direction from the configuration context.
-TrafficDirection getTrafficDirection()
-{
+TrafficDirection getTrafficDirection() {
   int64_t direction;
-  if (getValue({"listener_direction"}, &direction))
-  {
+  if (getValue({"listener_direction"}, &direction)) {
     return static_cast<TrafficDirection>(direction);
   }
   return TrafficDirection::Unspecified;
@@ -109,53 +98,32 @@ using google::protobuf::util::MessageToJsonString;
 // Custom-written and lenient struct parser.
 google::protobuf::util::Status extractNodeMetadata(
     const google::protobuf::Struct &metadata,
-    wasm::common::NodeInfo *node_info)
-{
-  for (const auto &it : metadata.fields())
-  {
-    if (it.first == "NAME")
-    {
+    wasm::common::NodeInfo *node_info) {
+  for (const auto &it : metadata.fields()) {
+    if (it.first == "NAME") {
       node_info->set_name(it.second.string_value());
-    }
-    else if (it.first == "NAMESPACE")
-    {
+    } else if (it.first == "NAMESPACE") {
       node_info->set_namespace_(it.second.string_value());
-    }
-    else if (it.first == "OWNER")
-    {
+    } else if (it.first == "OWNER") {
       node_info->set_owner(it.second.string_value());
-    }
-    else if (it.first == "WORKLOAD_NAME")
-    {
+    } else if (it.first == "WORKLOAD_NAME") {
       node_info->set_workload_name(it.second.string_value());
-    }
-    else if (it.first == "ISTIO_VERSION")
-    {
+    } else if (it.first == "ISTIO_VERSION") {
       node_info->set_istio_version(it.second.string_value());
-    }
-    else if (it.first == "MESH_ID")
-    {
+    } else if (it.first == "MESH_ID") {
       node_info->set_mesh_id(it.second.string_value());
-    }
-    else if (it.first == "LABELS")
-    {
+    } else if (it.first == "LABELS") {
       auto *labels = node_info->mutable_labels();
-      for (const auto &labels_it : it.second.struct_value().fields())
-      {
+      for (const auto &labels_it : it.second.struct_value().fields()) {
         (*labels)[labels_it.first] = labels_it.second.string_value();
       }
-    }
-    else if (it.first == "PLATFORM_METADATA")
-    {
+    } else if (it.first == "PLATFORM_METADATA") {
       auto *platform_metadata = node_info->mutable_platform_metadata();
-      for (const auto &platform_it : it.second.struct_value().fields())
-      {
+      for (const auto &platform_it : it.second.struct_value().fields()) {
         (*platform_metadata)[platform_it.first] =
             platform_it.second.string_value();
       }
-    }
-    else if (it.first == "GCP_PROJECT_ID")
-    {
+    } else if (it.first == "GCP_PROJECT_ID") {
       node_info->set_gcp_project_id(it.second.string_value());
     }
   }
@@ -164,14 +132,12 @@ google::protobuf::util::Status extractNodeMetadata(
 
 google::protobuf::util::Status extractNodeMetadataGeneric(
     const google::protobuf::Struct &metadata,
-    wasm::common::NodeInfo *node_info)
-{
+    wasm::common::NodeInfo *node_info) {
   google::protobuf::util::JsonOptions json_options;
   std::string metadata_json_struct;
   auto status =
       MessageToJsonString(metadata, &metadata_json_struct, json_options);
-  if (status != google::protobuf::util::Status::OK)
-  {
+  if (status != google::protobuf::util::Status::OK) {
     return status;
   }
   google::protobuf::util::JsonParseOptions json_parse_options;
@@ -181,11 +147,9 @@ google::protobuf::util::Status extractNodeMetadataGeneric(
 }
 
 google::protobuf::util::Status extractLocalNodeMetadata(
-    wasm::common::NodeInfo *node_info)
-{
+    wasm::common::NodeInfo *node_info) {
   google::protobuf::Struct node;
-  if (!getStructValue({"node", "metadata"}, &node))
-  {
+  if (!getStructValue({"node", "metadata"}, &node)) {
     return google::protobuf::util::Status(
         google::protobuf::util::error::Code::NOT_FOUND, "metadata not found");
   }
@@ -195,25 +159,21 @@ google::protobuf::util::Status extractLocalNodeMetadata(
 // Host header is used if use_host_header_fallback==true.
 // Normally it is ok to use host header within the mesh, but not at ingress.
 void populateHTTPRequestInfo(bool outbound, bool use_host_header_fallback,
-                             RequestInfo* request_info) {
+                             RequestInfo *request_info) {
   // TODO: switch to stream_info.requestComplete() to avoid extra compute.
   request_info->end_timestamp = getCurrentTimeNanoseconds();
 
   // Fill in request info.
   int64_t response_code = 0;
-  if (getValue({"response", "code"}, &response_code))
-  {
+  if (getValue({"response", "code"}, &response_code)) {
     request_info->response_code = response_code;
   }
 
   if (kGrpcContentTypes.count(getHeaderMapValue(HeaderMapType::RequestHeaders,
                                                 kContentTypeHeaderKey)
-                                  ->toString()) != 0)
-  {
+                                  ->toString()) != 0) {
     request_info->request_protocol = kProtocolGRPC;
-  }
-  else
-  {
+  } else {
     // TODO Add http/1.1, http/1.0, http/2 in a separate attribute.
     // http|grpc classification is compatible with Mixerclient
     request_info->request_protocol = kProtocolHTTP;
@@ -251,20 +211,16 @@ void populateHTTPRequestInfo(bool outbound, bool use_host_header_fallback,
 
   int64_t destination_port = 0;
 
-  if (outbound)
-  {
+  if (outbound) {
     getValue({"upstream", "port"}, &destination_port);
     getStringValue({"upstream", "uri_san_peer_certificate"},
                    &request_info->destination_principal);
     getStringValue({"upstream", "uri_san_local_certificate"},
                    &request_info->source_principal);
-  }
-  else
-  {
+  } else {
     getValue({"destination", "port"}, &destination_port);
     bool mtls = false;
-    if (getValue({"connection", "mtls"}, &mtls))
-    {
+    if (getValue({"connection", "mtls"}, &mtls)) {
       request_info->service_auth_policy =
           mtls ? ::Wasm::Common::ServiceAuthenticationPolicy::MutualTLS
                : ::Wasm::Common::ServiceAuthenticationPolicy::None;
@@ -283,25 +239,21 @@ void populateHTTPRequestInfo(bool outbound, bool use_host_header_fallback,
 
 google::protobuf::util::Status extractNodeMetadataValue(
     const google::protobuf::Struct &node_metadata,
-    google::protobuf::Struct *metadata)
-{
-  if (metadata == nullptr)
-  {
+    google::protobuf::Struct *metadata) {
+  if (metadata == nullptr) {
     return google::protobuf::util::Status(
         google::protobuf::util::error::INVALID_ARGUMENT,
         "metadata provided is null");
   }
   const auto key_it = node_metadata.fields().find("EXCHANGE_KEYS");
-  if (key_it == node_metadata.fields().end())
-  {
+  if (key_it == node_metadata.fields().end()) {
     return google::protobuf::util::Status(
         google::protobuf::util::error::INVALID_ARGUMENT,
         "metadata exchange key is missing");
   }
 
   const auto &keys_value = key_it->second;
-  if (keys_value.kind_case() != google::protobuf::Value::kStringValue)
-  {
+  if (keys_value.kind_case() != google::protobuf::Value::kStringValue) {
     return google::protobuf::util::Status(
         google::protobuf::util::error::INVALID_ARGUMENT,
         "metadata exchange key is not a string");
@@ -310,11 +262,9 @@ google::protobuf::util::Status extractNodeMetadataValue(
   // select keys from the metadata using the keys
   const std::set<std::string> keys =
       absl::StrSplit(keys_value.string_value(), ',', absl::SkipWhitespace());
-  for (auto key : keys)
-  {
+  for (auto key : keys) {
     const auto entry_it = node_metadata.fields().find(key);
-    if (entry_it == node_metadata.fields().end())
-    {
+    if (entry_it == node_metadata.fields().end()) {
       continue;
     }
     (*metadata->mutable_fields())[key] = entry_it->second;
@@ -323,5 +273,5 @@ google::protobuf::util::Status extractNodeMetadataValue(
   return google::protobuf::util::Status(google::protobuf::util::error::OK, "");
 }
 
-} // namespace Common
-} // namespace Wasm
+}  // namespace Common
+}  // namespace Wasm
