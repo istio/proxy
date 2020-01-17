rexengineering/istio-proxy

Add extra labels to sd logging (#2625)
* add extra fields into request info

* add logic to export extended request info

* add initial value

* update wasm file

* address comment

* fix

extensions/common/context.cc
@@ -294,6 +294,27 @@ void populateHTTPRequestInfo(bool outbound, bool use_host_header_fallback,
   }
 }
 
+void populateExtendedHTTPRequestInfo(RequestInfo* request_info) {
+  getValue({"source", "address"}, &request_info->source_address);
+  getValue({"destination", "address"}, &request_info->destination_address);
+
+  getValue({"request", "referer"}, &request_info->referer);
+  getValue({"request", "user_agent"}, &request_info->user_agent);
+  getValue({"request", "id"}, &request_info->request_id);
+  std::string trace_sampled;
+  if (getValue({"request", "headers", "x-b3-sampled"}, &trace_sampled) &&
+      !trace_sampled.empty() && trace_sampled != "0") {
+    getValue({"request", "headers", "x-b3-traceid"},
+             &request_info->b3_trace_id);
+    getValue({"request", "headers", "x-b3-spanid"}, &request_info->b3_span_id);
+    request_info->b3_trace_sampled = true;
+  }
+
+  getValue({"request", "url_path"}, &request_info->url_path);
+  getValue({"request", "host"}, &request_info->url_host);
+  getValue({"request", "scheme"}, &request_info->url_scheme);
+}
+
 void populateTCPRequestInfo(bool outbound, RequestInfo* request_info,
                             const std::string& destination_namespace) {
   // host_header_fallback is for HTTP/gRPC only.

istio/proxy

extensions/common/context.cc
@@ -289,6 +289,27 @@ void populateHTTPRequestInfo(bool outbound, bool use_host_header_fallback,
   getValue({"response", "total_size"}, &request_info->response_size);
 }
 
+void populateExtendedHTTPRequestInfo(RequestInfo* request_info) {
+  getStringValue({"source", "address"}, &request_info->source_address);
+  getStringValue({"destination", "address"}, &request_info->destination_address);
+
+  getStringValue({"request", "referer"}, &request_info->referer);
+  getStringValue({"request", "user_agent"}, &request_info->user_agent);
+  getStringValue({"request", "id"}, &request_info->request_id);
+  std::string trace_sampled;
+  if (getStringValue({"request", "headers", "x-b3-sampled"}, &trace_sampled) &&
+      !trace_sampled.empty() && trace_sampled != "0") {
+    getStringValue({"request", "headers", "x-b3-traceid"},
+             &request_info->b3_trace_id);
+    getStringValue({"request", "headers", "x-b3-spanid"}, &request_info->b3_span_id);
+    request_info->b3_trace_sampled = true;
+  }
+
+  getStringValue({"request", "url_path"}, &request_info->url_path);
+  getStringValue({"request", "host"}, &request_info->url_host);
+  getStringValue({"request", "scheme"}, &request_info->url_scheme);
+}
+
 google::protobuf::util::Status extractNodeMetadataValue(
     const google::protobuf::Struct& node_metadata,
     google::protobuf::Struct* metadata) {