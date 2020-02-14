0c11236971f5043c68db65a3522ba3e8b365d105
Travis Clarke <clarketm@google.com>
2020-02-14 11:19:19 -0800

    Update common-files@master

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -14,4 +14,4 @@
 
 # this repo is not on the container plan by default
 BUILD_WITH_CONTAINER ?= 0
-IMG = gcr.io/istio-testing/build-tools-proxy:master-2020-01-18T00-45-13
+IMG = gcr.io/istio-testing/build-tools-proxy:master-2020-02-14T13-09-14
