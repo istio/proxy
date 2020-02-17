6fb94e900514c527b03a822869ddb279b4964488
Mandar Jog <mjog@google.com>
2020-02-16 22:59:41 -0800

    WIP jwt_token to header

---
+++ extensions/jwt_header/plugin.cc
@@ -0,0 +1,127 @@
+/* Copyright 2019 Istio Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "extensions/jwt_header/plugin.h"
+
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
+#include "extensions/common/node_info.pb.h"
+#include "google/protobuf/util/json_util.h"
+
+#ifndef NULL_PLUGIN
+
+#include "base64.h"
+
+#else
+
+#include "common/common/base64.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Wasm {
+namespace JwtHeader {
+namespace Plugin {
+
+using namespace ::Envoy::Extensions::Common::Wasm::Null::Plugin;
+using NullPluginRegistry =
+    ::Envoy::Extensions::Common::Wasm::Null::NullPluginRegistry;
+
+NULL_PLUGIN_REGISTRY;
+
+#endif
+
+static RegisterContextFactory register_JwtHeader(
+    CONTEXT_FACTORY(PluginContext), ROOT_FACTORY(PluginRootContext));
+
+bool PluginRootContext::onConfigure(size_t) {
+  std::unique_ptr<WasmData> configuration = getConfiguration();
+  // Parse configuration JSON string.
+  JsonParseOptions json_options;
+  Status status =
+      JsonStringToMessage(configuration->toString(), &config_, json_options);
+  if (status != Status::OK) {
+    LOG_WARN(absl::StrCat("Cannot parse plugin configuration JSON string ",
+                          configuration->toString()));
+    return false;
+  }
+
+  if (config_.jwt_path().empty()) {
+    jwt_path_ = {"metadata", "jwt"};
+  } //else {
+    //jwt_path_ = std::initializer_list<StringView>(config_.jwt_path());
+  //}
+
+  return true;
+}
+
+FilterHeadersStatus PluginContext::onRequestHeaders(uint32_t) {
+  google::protobuf::Struct jwtPayloadStruct;
+
+  if (!getValue(rootContext()->jwt_path(), &jwtPayloadStruct)) {
+    LOG_INFO("no jwt metadata present");
+    return FilterHeadersStatus::Continue;
+  }
+
+  // Istio jwt filter adds exactly one entry to the map
+  // key: issuer which is not relevant here.
+  auto it = jwtPayloadStruct.fields().begin();
+  
+  if (jwtPayloadStruct.fields().end() == it) {
+    LOG_INFO("empty jwt metadata");
+    return FilterHeadersStatus::Continue;
+  }
+
+  auto jsonJwt = Base64::decodeWithoutPadding(it->second.string_value());
+  
+  if (jsonJwt.empty()) {
+    LOG_WARN("Invalid base64 in jwt metadata");
+    return FilterHeadersStatus::Continue;
+  }
+  
+  google::protobuf::Struct jwtStruct;
+  JsonParseOptions json_options;
+  auto status =
+      JsonStringToMessage(jsonJwt, &jwtStruct, json_options);
+
+  if (status != Status::OK) {
+    LOG_WARN(absl::StrCat("Cannot parse JSON string ",
+                          jsonJwt));
+    return FilterHeadersStatus::Continue;
+  }
+
+  for (const auto& mapping: rootContext()->config().header_map()){
+    const auto claim_it = jwtStruct.fields().find(mapping.second);
+    if (jwtStruct.fields().end() == claim_it) {
+      LOG_WARN(absl::StrCat("Claim ", mapping.second, " missing from ", jsonJwt));
+      continue;
+    }
+    if (WasmResult::Ok != addRequestHeader(mapping.first, claim_it->second.string_value())) {
+      LOG_WARN(absl::StrCat("Unable to set header ", mapping.first, " to ", claim_it->second.string_value()));
+    }
+  }
+
+
+  return FilterHeadersStatus::Continue;
+}
+
+#ifdef NULL_PLUGIN
+}  // namespace Plugin
+}  // namespace JwtHeader
+}  // namespace Wasm
+}  // namespace Extensions
+}  // namespace Envoy
+#endif
---
+++ extensions/jwt_header/plugin.h
@@ -0,0 +1,96 @@
+/* Copyright 2019 Istio Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "extensions/common/context.h"
+#include "extensions/jwt_header/config.pb.h"
+
+#ifndef NULL_PLUGIN
+
+#include <assert.h>
+#define ASSERT(_X) assert(_X)
+
+#include "proxy_wasm_intrinsics.h"
+
+static const std::string EMPTY_STRING;
+
+#else
+
+#include "extensions/common/wasm/null/null_plugin.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Wasm {
+namespace JwtHeader {
+namespace Plugin {
+
+using namespace Envoy::Extensions::Common::Wasm::Null::Plugin;
+
+using WasmResult = Envoy::Extensions::Common::Wasm::WasmResult;
+using NullPluginRegistry =
+    ::Envoy::Extensions::Common::Wasm::Null::NullPluginRegistry;
+
+#endif
+
+using google::protobuf::util::JsonParseOptions;
+using google::protobuf::util::Status;
+
+// PluginRootContext is the root context for all streams processed by the
+// thread. It has the same lifetime as the worker thread and acts as target for
+// interactions that outlives individual stream, e.g. timer, async calls.
+class PluginRootContext : public RootContext {
+ public:
+  PluginRootContext(uint32_t id, StringView root_id)
+      : RootContext(id, root_id) {}
+  ~PluginRootContext() = default;
+
+  bool onConfigure(size_t) override;
+  bool onStart(size_t) override { return true; };
+  void onTick() override{};
+
+  
+  std::initializer_list<StringView> jwt_path(){ return jwt_path_; };
+
+  const jwt_header::PluginConfig& config() {return config_; };
+ private:
+  // streamInfo().setDynamicMetadata("jwt", MessageUtil::keyValueStruct(issuer, payload))
+  // getProperty({"metadata", "jwt", issuer}, &value)
+  std::initializer_list<StringView> jwt_path_;
+  jwt_header::PluginConfig config_;
+  
+};
+
+// Per-stream context.
+class PluginContext : public Context {
+ public:
+  explicit PluginContext(uint32_t id, RootContext* root) : Context(id, root) {}
+
+  void onCreate() override{};
+  FilterHeadersStatus onRequestHeaders(uint32_t) override;
+
+ private:
+  inline PluginRootContext* rootContext() {
+    return dynamic_cast<PluginRootContext*>(this->root());
+  };
+};
+
+#ifdef NULL_PLUGIN
+}  // namespace Plugin
+}  // namespace JwtHeader
+}  // namespace Wasm
+}  // namespace Extensions
+}  // namespace Envoy
+#endif
