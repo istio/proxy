7bad8b962c29dd74487e11005e515af1ea6061ce
Joshua Blatt <jtblatt@gmail.com>
2020-03-04 13:06:55 -0800

    Revert "Fix node metadata fetch in stackdriver filter (#2683)"
    This reverts commit 15dd15b8096cf767aa9b9e5ca745e24792e1a2e2.

--- extensions/stackdriver/edges/mesh_edges_service_client.cc
+++ extensions/stackdriver/edges/mesh_edges_service_client.cc
@@ -16,7 +16,6 @@
 
 #include "extensions/stackdriver/edges/mesh_edges_service_client.h"
 
-#include "extensions/stackdriver/common/constants.h"
 #include "extensions/stackdriver/common/utils.h"
 #include "google/protobuf/util/time_util.h"
 
@@ -27,46 +26,6 @@ namespace Common {
 namespace Wasm {
 namespace Null {
 namespace Plugin {
-
-namespace {
-
-// StackdriverContextGraphHandler is used to inject x-goog-user-project header
-// into gRPC initial metadata. This is a work around since gRPC library is not
-// yet able to add that header automatically based on quota project from STS
-// token. This should not be needed after
-// https://github.com/grpc/grpc/issues/21225 is ready.
-class StackdriverContextGraphHandler
-    : public GrpcCallHandler<google::protobuf::Empty> {
- public:
-  StackdriverContextGraphHandler(const std::string& project_id)
-      : GrpcCallHandler(), project_id_(project_id) {}
-
-  void onCreateInitialMetadata() override {
-    addHeaderMapValue(
-        HeaderMapType::GrpcCreateInitialMetadata,
-        ::Extensions::Stackdriver::Common::kGoogleUserProjectHeaderKey,
-        project_id_);
-  }
-
-  void onSuccess(google::protobuf::Empty&&) override {
-    // TODO(douglas-reid): improve logging message.
-    logDebug(
-        "successfully sent MeshEdgesService ReportTrafficAssertionsRequest");
-  }
-
-  void onFailure(GrpcStatus status,
-                 std::unique_ptr<WasmData> message) override {
-    // TODO(douglas-reid): add retry (and other) logic
-    logWarn("MeshEdgesService ReportTrafficAssertionsRequest failure: " +
-            std::to_string(static_cast<int>(status)) + " " +
-            message->toString());
-  }
-
- private:
-  const std::string& project_id_;
-};
-
-}  // namespace
 
 using envoy::api::v2::core::GrpcService;
 using Envoy::Extensions::Common::Wasm::Null::Plugin::GrpcStatus;
@@ -92,8 +51,21 @@ using google::protobuf::util::TimeUtil;
 
 MeshEdgesServiceClientImpl::MeshEdgesServiceClientImpl(
     RootContext* root_context, const std::string& edges_endpoint,
-    const std::string& project_id, const std::string& sts_port)
-    : context_(root_context), project_id_(project_id) {
+    const std::string& sts_port)
+    : context_(root_context) {
+  success_callback_ = [](google::protobuf::Empty&&) {
+    // TODO(douglas-reid): improve logging message.
+    logDebug(
+        "successfully sent MeshEdgesService ReportTrafficAssertionsRequest");
+  };
+
+  failure_callback_ = [](GrpcStatus status, StringView message) {
+    // TODO(douglas-reid): add retry (and other) logic
+    logWarn("MeshEdgesService ReportTrafficAssertionsRequest failure: " +
+            std::to_string(static_cast<int>(status)) + " " +
+            std::string(message));
+  };
+
   GrpcService grpc_service;
   grpc_service.mutable_google_grpc()->set_stat_prefix("mesh_edges");
   if (edges_endpoint.empty()) {
@@ -126,10 +98,9 @@ MeshEdgesServiceClientImpl::MeshEdgesServiceClientImpl(
 
 void MeshEdgesServiceClientImpl::reportTrafficAssertions(
     const ReportTrafficAssertionsRequest& request) const {
-  auto handler = std::make_unique<StackdriverContextGraphHandler>(project_id_);
-  context_->grpcCallHandler(grpc_service_, kMeshEdgesService,
-                            kReportTrafficAssertions, request,
-                            kDefaultTimeoutMillisecond, std::move(handler));
+  context_->grpcSimpleCall(
+      grpc_service_, kMeshEdgesService, kReportTrafficAssertions, request,
+      kDefaultTimeoutMillisecond, success_callback_, failure_callback_);
 };
 
 }  // namespace Edges
--- extensions/stackdriver/stackdriver.cc
+++ extensions/stackdriver/stackdriver.cc
@@ -115,7 +115,7 @@ int getExportInterval() {
 // provided or "0" is provided, emtpy will be returned.
 std::string getSTSPort() {
   std::string sts_port;
-  if (getStringValue({"node", "metadata", kSTSPortKey}, &sts_port) &&
+  if (getValue({"node", "metadata", kSTSPortKey}, &sts_port) &&
       sts_port != "0") {
     return sts_port;
   }
@@ -146,17 +146,11 @@ bool StackdriverRootContext::onConfigure(
   direction_ = ::Wasm::Common::getTrafficDirection();
   use_host_header_fallback_ = !config_.disable_host_header_fallback();
   std::string sts_port = getSTSPort();
-  std::string project_id;
-  const auto& platform_metadata = local_node_info_.platform_metadata();
-  const auto project_iter = platform_metadata.find(kGCPProjectKey);
-  if (project_iter != platform_metadata.end()) {
-    project_id = project_iter->second;
-  }
   if (!logger_) {
     // logger should only be initiated once, for now there is no reason to
     // recreate logger because of config update.
-    auto exporter = std::make_unique<ExporterImpl>(this, getLoggingEndpoint(),
-                                                   project_id, sts_port);
+    auto exporter =
+        std::make_unique<ExporterImpl>(this, getLoggingEndpoint(), sts_port);
     // logger takes ownership of exporter.
     logger_ = std::make_unique<Logger>(local_node_info_, std::move(exporter));
   }
@@ -165,7 +159,7 @@ bool StackdriverRootContext::onConfigure(
     // edge reporter should only be initiated once, for now there is no reason
     // to recreate edge reporter because of config update.
     auto edges_client = std::make_unique<MeshEdgesServiceClientImpl>(
-        this, getMeshTelemetryEndpoint(), project_id, sts_port);
+        this, getMeshTelemetryEndpoint(), sts_port);
     edge_reporter_ = std::make_unique<EdgeReporter>(local_node_info_,
                                                     std::move(edges_client));
   }
