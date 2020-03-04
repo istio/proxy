73f240a29bece92a8882a36893ccce07b4a54664
Nupur Garg <37600866+gargnupur@users.noreply.github.com>
2020-03-04 00:49:53 -0800

    [Release-1.5] Remove not getting TCP Metrics for unknown peers and Add Context in o… (#2746)
    * Remove not getting TCP Metrics for unknown peers and Add Context in onTick calls (#2742)
    
    * Remove not getting TCP Metrics for unknown peers
    
    * Add check for unhealthy  upstream
    
    * Add Context in OnTick Calls
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Update  Proxy SHA
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Remove using that was added for getCOntext, its not needed and makes build_wasm fail
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Update SHA
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Fix test
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Fix test
    
    Signed-off-by: gargnupur <gargnupur@google.com>

--- src/envoy/tcp/sni_verifier/sni_verifier_test.cc
+++ src/envoy/tcp/sni_verifier/sni_verifier_test.cc
@@ -55,7 +55,7 @@ TEST(SniVerifierTest, MaxClientHelloSize) {
 
 class SniVerifierFilterTest : public testing::Test {
  protected:
-  static constexpr size_t TLS_MAX_CLIENT_HELLO = 200;
+  static constexpr size_t TLS_MAX_CLIENT_HELLO = 250;
 
   void SetUp() override {
     store_ = std::make_unique<Stats::IsolatedStoreImpl>();
@@ -72,7 +72,8 @@ class SniVerifierFilterTest : public testing::Test {
   void runTestForClientHello(std::string outer_sni, std::string inner_sni,
                              Network::FilterStatus expected_status,
                              size_t data_installment_size = UINT_MAX) {
-    auto client_hello = Tls::Test::generateClientHello(inner_sni, "");
+    auto client_hello = Tls::Test::generateClientHello(
+        TLS1_VERSION, TLS1_3_VERSION, inner_sni, "");
     runTestForData(outer_sni, client_hello, expected_status,
                    data_installment_size);
   }
