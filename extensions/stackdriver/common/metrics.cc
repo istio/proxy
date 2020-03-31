26e4bb1b6694f59b32f0e1bbd24bcbaadc6b1d42
Pengyuan Bian <bianpengyuan@google.com>
2020-03-31 03:51:07 +0000

    fix format

--- extensions/stackdriver/common/metrics.cc
+++ extensions/stackdriver/common/metrics.cc
@@ -30,15 +30,18 @@ namespace Stackdriver {
 namespace Common {
 
 // NOTE: every call to this function will alloc a new metric object. Though this
-// object is the same in all calls, it cannot be extracted out to a global variable.
-// The metric object caches all fully resolved metrics and won't make define
-// metrics abi call when the same resolved metrics are defined. In Null VM case,
-// since global objects are shared between root contexts in base VM and in thread
-// local VM, and the same metrics definition code path will excercised in both VMs (we don't really have a way to distinguish base VM and thread local VM), the cache will make thread local root context miss metric definition
-// at host side. In real Wasm VM, this can be extracted to a global var.
+// object is the same in all calls, it cannot be extracted out to a global
+// variable. The metric object caches all fully resolved metrics and won't make
+// define metrics abi call when the same resolved metrics are defined. In Null
+// VM case, since global objects are shared between root contexts in base VM and
+// in thread local VM, and the same metrics definition code path will excercised
+// in both VMs (we don't really have a way to distinguish base VM and thread
+// local VM), the cache will make thread local root context miss metric
+// definition at host side. In real Wasm VM, this can be extracted to a global
+// var.
 uint32_t newExportCallMetric(const std::string& type, bool success) {
   Metric export_call(MetricType::Counter, "stackdriver_export_call",
-                    {MetricTag{"wasm_filter", MetricTag::TagType::String},
+                     {MetricTag{"wasm_filter", MetricTag::TagType::String},
                       MetricTag{"type", MetricTag::TagType::String},
                       MetricTag{"success", MetricTag::TagType::Bool}});
 
