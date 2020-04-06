b5b0739e37ad63e1c91b9072534bd626aac02b9d
Kuat Yessenov <kuat@google.com>
2020-04-06 13:55:49 -0700

    fix wasm build
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- extensions/stats/build_wasm.sh
+++ extensions/stats/build_wasm.sh
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 set -e
+set -x
 
 docker run -e uid="$(id -u)" -e gid="$(id -g)" -v $PWD:/work -w /work -v $(realpath $PWD/../../extensions):/work/extensions gcr.io/istio-testing/wasmsdk:v3 bash /build_wasm.sh
 rmdir extensions
