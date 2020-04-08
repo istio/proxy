aead494eb4933cc30f57e78bb8daab7f81c9df0e
Pengyuan Bian <bianpengyuan@google.com>
2020-04-08 14:54:45 -0700

    Make wasm sdk image configurable (#2784)
    * make wasm sdk image configurable
    
    * fix
    
    * update wasm gen script
    
    * extract out wasm module build script to a library
    
    * add build_wasm function
    
    * use bazel info workspace to get workspace dir in wasm build script

--- extensions/stats/build_wasm.sh
+++ extensions/stats/build_wasm.sh
@@ -17,5 +17,7 @@
 set -e
 set -x
 
-docker run -e uid="$(id -u)" -e gid="$(id -g)" -v $PWD:/work -w /work -v $(realpath $PWD/../../extensions):/work/extensions gcr.io/istio-testing/wasmsdk:v3 bash /build_wasm.sh
-rmdir extensions
+WS=$(bazel info workspace)
+source ${WS}/scripts/build_wasm.inc
+
+build_wasm
