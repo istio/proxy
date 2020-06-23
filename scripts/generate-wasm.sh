62b2707a0dd48d795bd224c84b5504f9d9ae7fb8
Pengyuan Bian <bianpengyuan@google.com>
2020-06-23 22:51:40 +0000

    update

--- scripts/generate-wasm.sh
+++ scripts/generate-wasm.sh
@@ -64,7 +64,7 @@ if [[ "$(docker images -q ${WASM_SDK_IMAGE}:${WASM_SDK_TAG} 2> /dev/null)" == ""
   TMP_DIR=$(mktemp -d -t ${ENVOY_REPO}-XXXXXXXXXX)
   trap "rm -rf ${TMP_DIR}" EXIT
 
-  if [[ -z ${ENVOY_DIR} ]]; then
+  if [[ -z "${ENVOY_DIR}" ]]; then
     cd ${TMP_DIR}
     git clone https://github.com/${ENVOY_ORG}/${ENVOY_REPO}
     cd ${ENVOY_REPO}
