bc6bb6eb2b83c00a9f06995ec86c0f4d25227645
Pengyuan Bian <bianpengyuan@google.com>
2020-06-24 00:13:34 -0700

    [release-1.5] cherry-pick wasm build fixes (#2882)
    * cherry-pick wasm build fixes
    
    * update

--- scripts/generate-wasm.sh
+++ scripts/generate-wasm.sh
@@ -64,16 +64,23 @@ if [[ "$(docker images -q ${IMAGE}:${TAG} 2> /dev/null)" == "" ]]; then
   TMP_DIR=$(mktemp -d -t istio-envoy-XXXXXXXXXX)
   trap "rm -rf ${TMP_DIR}" EXIT
 
+  if [[ -z "${ENVOY_DIR}" ]]; then
+    cd ${TMP_DIR}
+    git clone https://github.com/${ENVOY_ORG}/${ENVOY_REPO}
+    cd ${ENVOY_REPO}
+  else
+    # ENVOY_DIR is absolute path of local envoy dir used for Wasm build.
+    cp -r ${ENVOY_DIR} ${TMP_DIR}
+    cd ${TMP_DIR}/$(basename "${ENVOY_DIR}")
+  fi
+
   # Check out to envoy SHA
-  cd ${TMP_DIR}
-  git clone https://github.com/${ENVOY_PROJECT}/${ENVOY_REPO}
-  cd ${ENVOY_REPO}
   git checkout ${ENVOY_SHA}
 
   # Rebuild and push
   cd api/wasm/cpp && docker build -t ${IMAGE}:${TAG} -f Dockerfile-sdk .
   if [[ ${PUSH_DOCKER_IMAGE} == 1 ]]; then
-    docker push ${IMAGE}:${TAG} || "fail to push to gcr.io/istio-testing hub"
+    docker push ${IMAGE}:${TAG} || echo "fail to push to gcr.io/istio-testing hub"
   fi
 fi
 
