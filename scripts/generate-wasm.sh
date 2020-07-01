cce8271d83d2deaf68e3a971be5ef3b35aac66d3
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2020-07-01 16:11:21 -0600

    Copy generate-wasm.sh from release-1.6 branch

--- scripts/generate-wasm.sh
+++ scripts/generate-wasm.sh
@@ -42,26 +42,26 @@ while getopts bpcd: arg ; do
   esac
 done
 
-# Get SHA of istio/envoy repo
+# Get SHA of envoy-wasm repo
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 WORKSPACE=${ROOT}/WORKSPACE
 ENVOY_SHA="$(grep -Pom1 "^ENVOY_SHA = \"\K[a-zA-Z0-9]{40}" "${WORKSPACE}")"
-ENVOY_PROJECT="$(grep -Pom1 "^ENVOY_PROJECT = \"\K[a-zA-Z0-9-]*" "${WORKSPACE}")"
-ENVOY_REPO="$(grep -Pom1 "^ENVOY_REPO = \"\K[a-zA-Z0-9]*" "${WORKSPACE}")"
-IMAGE=gcr.io/istio-testing/wasmsdk
-TAG=${ENVOY_SHA}
+ENVOY_ORG="$(grep -Pom1 "^ENVOY_ORG = \"\K[a-zA-Z-]+" "${WORKSPACE}")"
+ENVOY_REPO="$(grep -Pom1 "^ENVOY_REPO = \"\K[a-zA-Z-]+" "${WORKSPACE}")"
+WASM_SDK_IMAGE=${WASM_SDK_IMAGE:=gcr.io/istio-testing/wasmsdk}
+export WASM_SDK_TAG=${ENVOY_SHA}
 
 # Try pull wasm builder image.
-docker pull ${IMAGE}:${TAG} || echo "${IMAGE}:${TAG} does not exist"
+docker pull ${WASM_SDK_IMAGE}:${WASM_SDK_TAG} || echo "${WASM_SDK_IMAGE}:${WASM_SDK_TAG} does not exist"
 
 # If image does not exist, try build it
-if [[ "$(docker images -q ${IMAGE}:${TAG} 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q ${WASM_SDK_IMAGE}:${WASM_SDK_TAG} 2> /dev/null)" == "" ]]; then
   if [[ ${BUILD_CONTAINER} == 0 ]]; then
     echo "no builder image to compile wasm. Add `-b` option to create the builder image"
     exit 1
   fi
-  # Clone istio/envoy repo and checkout to that SHA
-  TMP_DIR=$(mktemp -d -t istio-envoy-XXXXXXXXXX)
+  # Clone envoy-wasm repo and checkout to that SHA
+  TMP_DIR=$(mktemp -d -t ${ENVOY_REPO}-XXXXXXXXXX)
   trap "rm -rf ${TMP_DIR}" EXIT
 
   if [[ -z "${ENVOY_DIR}" ]]; then
@@ -78,15 +78,13 @@ if [[ "$(docker images -q ${IMAGE}:${TAG} 2> /dev/null)" == "" ]]; then
   git checkout ${ENVOY_SHA}
 
   # Rebuild and push
-  cd api/wasm/cpp && docker build -t ${IMAGE}:${TAG} -f Dockerfile-sdk .
+  cd api/wasm/cpp && docker build -t ${WASM_SDK_IMAGE}:${WASM_SDK_TAG} -f Dockerfile-sdk .
   if [[ ${PUSH_DOCKER_IMAGE} == 1 ]]; then
-    docker push ${IMAGE}:${TAG} || echo "fail to push to gcr.io/istio-testing hub"
+    docker push ${WASM_SDK_IMAGE}:${WASM_SDK_TAG} || echo "fail to push to ${WASM_SDK_IMAGE} hub"
   fi
 fi
 
 # Regenerate all wasm plugins and compare diffs
-# Tag image to v3, which is what used by all build wasm script.
-docker tag ${IMAGE}:${TAG} ${IMAGE}:v3
 cd ${ROOT}
 find . -name "*.wasm" -type f -delete
 make build_wasm
@@ -99,6 +97,7 @@ if [ -n "${DST_BUCKET}" ]; then
   TMP_WASM=$(mktemp -d -t wasm-plugins-XXXXXXXXXX)
   trap "rm -rf ${TMP_WASM}" EXIT
   for i in `find . -name "*.wasm" -type f`; do
+    ls -lh ${i}
     # Get name of the plugin
     PLUGIN_NAME=$(basename $(dirname ${i}))
     # Rename the plugin file and generate sha256 for it
@@ -107,7 +106,7 @@ if [ -n "${DST_BUCKET}" ]; then
     SHA256_PATH="${WASM_PATH}.sha256"
     cp ${i} ${WASM_PATH}
     sha256sum "${WASM_PATH}" > "${SHA256_PATH}"
-
+    
     # push wasm files and sha to the given bucket
     gsutil stat "${DST_BUCKET}/${WASM_NAME}" \
       && { echo "WASM file ${WASM_NAME} already exist"; continue; } \
