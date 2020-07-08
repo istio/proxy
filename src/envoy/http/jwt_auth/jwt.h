909546c166d9316a8eb741de8da98e6164c6fc8d
Shikugawa <Shikugawa@gmail.com>
2020-07-08 16:16:06 +0900

    fix

--- src/envoy/http/jwt_auth/jwt.h
+++ src/envoy/http/jwt_auth/jwt.h
@@ -110,8 +110,6 @@ enum class Status {
 
 std::string StatusToString(Status status);
 
-std::string Base64UrlDecode(std::string input);
-
 // Base class to keep the status that represents "OK" or the first failure
 // reason
 class WithStatus {
