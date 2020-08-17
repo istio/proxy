d3f80851b38b87ec73724748135fbfeb77585056
Kuat Yessenov <kuat@google.com>
2020-08-17 10:11:23 -0700

    add envoy segfault handling
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- test/envoye2e/stats_plugin/stats_test.go
+++ test/envoye2e/stats_plugin/stats_test.go
@@ -369,15 +369,14 @@ func TestAttributeGen(t *testing.T) {
 func TestStatsParserRegression(t *testing.T) {
 	// This is a regression test for https://github.com/envoyproxy/envoy-wasm/issues/497
 	params := driver.NewTestParams(t, map[string]string{
-		"StatsFilterCode": "inline_string: \"envoy.wasm.stats\"",
-		"WasmRuntime":     "envoy.wasm.runtime.null",
-		"StatsConfig":     driver.LoadTestData("testdata/bootstrap/stats.yaml.tmpl"),
+		"StatsFilterCode":         "inline_string: \"envoy.wasm.stats\"",
+		"WasmRuntime":             "envoy.wasm.runtime.null",
+		"StatsConfig":             driver.LoadTestData("testdata/bootstrap/stats.yaml.tmpl"),
+		"ClientHTTPFilters":       driver.LoadTestData("testdata/filters/stats_outbound.yaml.tmpl"),
+		"StatsFilterClientConfig": "{}",
 	}, envoye2e.ProxyE2ETests)
-	params.Vars["StatsFilterClientConfig"] = "{}"
-	params.Vars["ClientHTTPFilters"] = params.LoadTestData("testdata/filters/stats_outbound.yaml.tmpl")
 	listener0 := params.LoadTestData("testdata/listener/client.yaml.tmpl")
-	params.Vars["StatsFilterClientConfig"] = driver.LoadTestJSON("testdata/stats/client_config_customized.yaml")
-	params.Vars["ClientHTTPFilters"] = params.LoadTestData("testdata/filters/stats_outbound.yaml.tmpl")
+	params.Vars["StatsFilterClientConfig"] = driver.LoadTestJSON("testdata/stats/client_config_customized.yaml.tmpl")
 	listener1 := params.LoadTestData("testdata/listener/client.yaml.tmpl")
 	if err := (&driver.Scenario{
 		[]driver.Step{
