1ff9a6b27d9ebc25df72f531f71f2d4d2f8f5f7f
Yuchen Dai <silentdai@gmail.com>
2021-04-10 15:14:22 -0700

    detect downstream first in tcp stream
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- extensions/stackdriver/stackdriver.h
+++ extensions/stackdriver/stackdriver.h
@@ -209,6 +209,9 @@ class StackdriverContext : public Context {
 
   // Called on onData call, so counting the data that is received.
   FilterStatus onDownstreamData(size_t size, bool) override {
+    if (!downstream_first_.has_value() && is_tcp_ &&  size > 0) {
+      downstream_first_ = true;
+    }
     if (!is_initialized_) {
       return FilterStatus::Continue;
     }
@@ -219,6 +222,9 @@ class StackdriverContext : public Context {
   }
   // Called on onWrite call, so counting the data that is sent.
   FilterStatus onUpstreamData(size_t size, bool) override {
+    if (!downstream_first_.has_value() && is_tcp_ && size > 0) {
+      downstream_first_ = false;
+    }
     if (!is_initialized_) {
       return FilterStatus::Continue;
     }
@@ -236,6 +242,7 @@ class StackdriverContext : public Context {
   bool is_tcp_;
   uint32_t context_id_;
   const bool is_initialized_;
+  absl::optional<bool> downstream_data_first_{};
 };
 
 class StackdriverOutboundRootContext : public StackdriverRootContext {
