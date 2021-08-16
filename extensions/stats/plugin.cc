b977f763c6a548991b16d5a3f41855db4168661f
刘训灼 <mixdeers@gmail.com>
2021-08-16 16:32:30 +0800

    fix: handle result to unknown when istio_version is missing

--- extensions/stats/plugin.cc
+++ extensions/stats/plugin.cc
@@ -450,9 +450,9 @@ bool PluginRootContext::initializeDimensions(const json& j) {
   Metric build(MetricType::Gauge, absl::StrCat(stat_prefix, "build"),
                {MetricTag{"component", MetricTag::TagType::String},
                 MetricTag{"tag", MetricTag::TagType::String}});
+  std::string istio_version = flatbuffers::GetString(local_node.istio_version()) == "" ? absl::StrCat(unknown, ";") : absl::StrCat(istio_version, ";");
   build.record(
-      1, "proxy",
-      absl::StrCat(flatbuffers::GetString(local_node.istio_version()), ";"));
+      1, "proxy", istio_version);
   return true;
 }
 
