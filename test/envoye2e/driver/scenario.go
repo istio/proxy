4c1cb4929bb7a7d01d1dbc7ab2b8fe3cc5b25695
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2021-11-12 09:59:35 -0700

    Run goimports

--- test/envoye2e/driver/scenario.go
+++ test/envoye2e/driver/scenario.go
@@ -24,10 +24,10 @@ import (
 	"text/template"
 	"time"
 
-	"sigs.k8s.io/yaml"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	yamlv2 "gopkg.in/yaml.v2"
+	"sigs.k8s.io/yaml"
 
 	"istio.io/proxy/test/envoye2e/env"
 )
