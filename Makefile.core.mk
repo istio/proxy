78bf60809e84083b305e6afc6c35afcc5994ce9e
Pengyuan Bian <bianpengyuan@google.com>
2022-02-25 04:14:54 +0000

    update.

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -72,8 +72,8 @@ CENTOS_BUILD_ARGS ?= --cxxopt -D_GLIBCXX_USE_CXX11_ABI=1 --cxxopt -DENVOY_IGNORE
 # TODO can we do some sort of regex?
 CENTOS_BAZEL_TEST_TARGETS ?= ${BAZEL_TARGETS} -tools/deb/... -tools/docker/... \
                              -extensions:stats.wasm -extensions:metadata_exchange.wasm -extensions:attributegen.wasm \
-							 -extensions:push_wasm_image_attributegen -extensions:push_wasm_image_metadata_exchange -extensions:push_wasm_image_stats \
-							 -extensions:wasm_image_attributegen -extensions:wasm_image_metadata_exchange -extensions:wasm_image_stats
+                             -extensions:push_wasm_image_attributegen -extensions:push_wasm_image_metadata_exchange -extensions:push_wasm_image_stats \
+                             -extensions:wasm_image_attributegen -extensions:wasm_image_metadata_exchange -extensions:wasm_image_stats
 
 build:
 	export PATH=$(PATH) CC=$(CC) CXX=$(CXX) && \
