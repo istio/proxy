c004aac891bd205340747857a4abcbb270eb24b0
Douglas Reid <dougreid@google.com>
2022-07-08 00:08:51 +0000

    telemetry: add support in stats filter for ambient server pep mode
    This PR establishes an extra config parameter for the stats mode to
    allow local node info mapping on a per request basis (pulling from host
    metadata set on the target endpoint). This is required for Ambient
    Server PEP operation because upstream metadata will not be available
    entirely from the Envoy node metadata (as it was with sidecar
    operations). The control plane _should_ still provide the required
    metadata for the endpoint.
    
    Note: at present, there is no mapping support for other pod labels or
    even 'app' and 'version' labels, as those are not supplied in the host
    metadata. if more upstream metadata is required, beyond what is
    currently available, more work is likely necessary. There may also be
    work required to properly convey service account info, etc.

--- extensions/stats/plugin.cc
+++ extensions/stats/plugin.cc
@@ -502,6 +502,13 @@ bool PluginRootContext::configure(size_t configuration_size) {
     return false;
   }
 
+  auto mode = JsonGetField<std::string_view>(j, "metadata_mode").value_or("");
+  if (mode == "AMBIENT_PEP_SERVER_MODE") {
+    metadata_mode_ = MetadataMode::ServerPep;
+  } else {
+    metadata_mode_ = MetadataMode::Default;
+  }
+
   // TODO: rename to reporting_duration
   uint32_t tcp_report_duration_milis = kDefaultTCPReportDurationMilliseconds;
   auto tcp_reporting_duration_field =
@@ -622,6 +629,21 @@ void PluginRootContext::report(::Wasm::Common::RequestInfo& request_info,
         ::Wasm::Common::populateGRPCInfo(&request_info);
       }
     }
+  }
+
+  if (metadata_mode_ == MetadataMode::ServerPep) {
+    // in PEP Server mode, we must remap the "local" node info per request
+    // as the proxy is no longer serving a single workload
+    auto detached = Wasm::Common::extractEmptyNodeFlatBuffer();
+
+    flatbuffers::FlatBufferBuilder fbb;
+    if (Wasm::Common::extractPeerMetadataFromUpstreamHostMetadata(fbb)) {
+      detached = fbb.Release();
+    }
+
+    const auto& node =
+        *flatbuffers::GetRoot<::Wasm::Common::FlatNode>(detached.data());
+    map_node(istio_dimensions_, false, node);
   }
 
   map(istio_dimensions_, outbound_, peer_node_info.get(), request_info);
