220a77182b9926a3e62ec4ceaa2b2d2c5cead5df
Kuat Yessenov <kuat@google.com>
2022-11-11 22:26:32 +0000

    lint
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- source/extensions/filters/network/istio_authn/config_test.cc
+++ source/extensions/filters/network/istio_authn/config_test.cc
@@ -15,12 +15,10 @@
 
 #include "source/extensions/filters/network/istio_authn/config.h"
 
-#include "source/common/buffer/buffer_impl.h"
-#include "source/common/stream_info/filter_state_impl.h"
-
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
+#include "source/common/buffer/buffer_impl.h"
+#include "source/common/stream_info/filter_state_impl.h"
 #include "test/mocks/network/mocks.h"
 #include "test/mocks/ssl/mocks.h"
 
@@ -33,38 +31,41 @@ namespace NetworkFilters {
 namespace IstioAuthn {
 
 TEST(Principal, Basic) {
-  const std::string value1 = "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
+  const std::string value1 =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
   auto p1 = Principal(value1);
   EXPECT_EQ(p1.serializeAsString(), absl::make_optional<std::string>(value1));
   EXPECT_EQ(p1.principal(), value1);
 
-  const std::string value2 = "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
+  const std::string value2 =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
   auto p2 = Principal(value2);
   EXPECT_NE(p1.hash(), p2.hash());
 }
 
 TEST(Principal, GetPrincipals) {
-  const std::string peer = "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
-  const std::string local = "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
-  StreamInfo::FilterStateImpl filter_state(StreamInfo::FilterState::LifeSpan::Connection);
+  const std::string peer =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
+  const std::string local =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
+  StreamInfo::FilterStateImpl filter_state(
+      StreamInfo::FilterState::LifeSpan::Connection);
   {
     const auto info = getPrincipals(filter_state);
     EXPECT_EQ(info.peer, "");
     EXPECT_EQ(info.local, "");
   }
-  filter_state.setData(
-              PeerPrincipalKey, std::make_shared<Principal>(peer),
-              StreamInfo::FilterState::StateType::ReadOnly,
-              StreamInfo::FilterState::LifeSpan::Connection);
+  filter_state.setData(PeerPrincipalKey, std::make_shared<Principal>(peer),
+                       StreamInfo::FilterState::StateType::ReadOnly,
+                       StreamInfo::FilterState::LifeSpan::Connection);
   {
     const auto info = getPrincipals(filter_state);
     EXPECT_EQ(info.peer, peer);
     EXPECT_EQ(info.local, "");
   }
-  filter_state.setData(
-              LocalPrincipalKey, std::make_shared<Principal>(local),
-              StreamInfo::FilterState::StateType::ReadOnly,
-              StreamInfo::FilterState::LifeSpan::Connection);
+  filter_state.setData(LocalPrincipalKey, std::make_shared<Principal>(local),
+                       StreamInfo::FilterState::StateType::ReadOnly,
+                       StreamInfo::FilterState::LifeSpan::Connection);
   {
     const auto info = getPrincipals(filter_state);
     EXPECT_EQ(info.peer, peer);
@@ -92,16 +93,20 @@ TEST(IstioAuthnFilter, CallbacksWithSsl) {
   filter.initializeReadFilterCallbacks(callbacks);
 
   auto ssl = std::make_shared<Ssl::MockConnectionInfo>();
-  const std::string peer = "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
-  const std::string local = "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
+  const std::string peer =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
+  const std::string local =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
   std::vector<std::string> peer_sans{peer};
   std::vector<std::string> local_sans{local};
   EXPECT_CALL(callbacks.connection_, ssl()).WillRepeatedly(Return(ssl));
   EXPECT_CALL(*ssl, peerCertificatePresented()).WillRepeatedly(Return(true));
   EXPECT_CALL(*ssl, uriSanPeerCertificate()).WillRepeatedly(Return(peer_sans));
-  EXPECT_CALL(*ssl, uriSanLocalCertificate()).WillRepeatedly(Return(local_sans));
+  EXPECT_CALL(*ssl, uriSanLocalCertificate())
+      .WillRepeatedly(Return(local_sans));
   callbacks.connection_.raiseEvent(Network::ConnectionEvent::Connected);
-  const auto info = getPrincipals(*callbacks.connection_.stream_info_.filter_state_);
+  const auto info =
+      getPrincipals(*callbacks.connection_.stream_info_.filter_state_);
   EXPECT_EQ(info.peer, peer);
   EXPECT_EQ(info.local, local);
 }
@@ -112,16 +117,20 @@ TEST(IstioAuthnFilter, CallbacksWithSslMultipleSAN) {
   filter.initializeReadFilterCallbacks(callbacks);
 
   auto ssl = std::make_shared<Ssl::MockConnectionInfo>();
-  const std::string spiffe1 = "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
-  const std::string spiffe2 = "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
+  const std::string spiffe1 =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account1";
+  const std::string spiffe2 =
+      "spiffe://cluster.local/ns/my-namespace/sa/my-account2";
   std::vector<std::string> peer_sans{"test1.com", spiffe1, spiffe2};
   std::vector<std::string> local_sans{"test2.com", "test3.com"};
   EXPECT_CALL(callbacks.connection_, ssl()).WillRepeatedly(Return(ssl));
   EXPECT_CALL(*ssl, peerCertificatePresented()).WillRepeatedly(Return(true));
   EXPECT_CALL(*ssl, uriSanPeerCertificate()).WillRepeatedly(Return(peer_sans));
-  EXPECT_CALL(*ssl, uriSanLocalCertificate()).WillRepeatedly(Return(local_sans));
+  EXPECT_CALL(*ssl, uriSanLocalCertificate())
+      .WillRepeatedly(Return(local_sans));
   callbacks.connection_.raiseEvent(Network::ConnectionEvent::Connected);
-  const auto info = getPrincipals(*callbacks.connection_.stream_info_.filter_state_);
+  const auto info =
+      getPrincipals(*callbacks.connection_.stream_info_.filter_state_);
   EXPECT_EQ(info.peer, spiffe1);
   EXPECT_EQ(info.local, "");
 }
