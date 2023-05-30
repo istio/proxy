e119d81bd2d84170d23c788935865504a92e380b
hejianpeng <hejianpeng2@huawei.com>
2023-05-30 20:16:49 +0800

    IgnoreUnexported

--- test/envoye2e/driver/stats.go
+++ test/envoye2e/driver/stats.go
@@ -21,6 +21,7 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/expfmt"
 
@@ -92,7 +93,7 @@ func (me *ExactStat) Matches(params *Params, that *dto.MetricFamily) error {
 	metric := &dto.MetricFamily{}
 	params.LoadTestProto(me.Metric, metric)
 
-	if diff := cmp.Diff(metric, that); diff != "" {
+	if diff := cmp.Diff(metric, that, cmpopts.IgnoreUnexported()); diff != "" {
 		return fmt.Errorf("diff: %v, got: %v, want: %v", diff, that, metric)
 	}
 	return nil
@@ -110,7 +111,7 @@ func (me *PartialStat) Matches(params *Params, that *dto.MetricFamily) error {
 	for _, wm := range metric.Metric {
 		found := false
 		for _, gm := range that.Metric {
-			if diff := cmp.Diff(wm, gm); diff != "" {
+			if diff := cmp.Diff(wm, gm, cmpopts.IgnoreUnexported()); diff != "" {
 				continue
 			}
 			found = true
