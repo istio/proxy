cd5b9e381bef433b1195afea756e13abd087a3cc
Kuat Yessenov <kuat@google.com>
2023-07-06 20:29:41 +0000

    wip
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- test/envoye2e/driver/otel.go
+++ test/envoye2e/driver/otel.go
@@ -19,11 +19,16 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
 	collogspb "go.opentelemetry.io/proto/otlp/collector/logs/v1"
 	colmetricspb "go.opentelemetry.io/proto/otlp/collector/metrics/v1"
+	metricspb "go.opentelemetry.io/proto/otlp/metrics/v1"
+
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/testing/protocmp"
 )
 
 type OtelLogs struct {
@@ -32,6 +37,13 @@ type OtelLogs struct {
 
 type OtelMetrics struct {
 	colmetricspb.UnimplementedMetricsServiceServer
+	finished bool
+	verify   verify
+}
+
+type verify struct {
+	want map[string]*metricspb.Metric
+	done chan struct{}
 }
 
 type Otel struct {
@@ -39,16 +51,61 @@ type Otel struct {
 	OtelMetrics
 	grpc *grpc.Server
 	Port uint16
+	// Metrics contains the expected (total) metric protos. Client must produce it exactly.
+	Metrics []string
 }
 
 func (x *OtelLogs) Export(ctx context.Context, req *collogspb.ExportLogsServiceRequest) (*collogspb.ExportLogsServiceResponse, error) {
-	log.Printf("log=%s\n", proto.MarshalTextString(req))
+	//log.Printf("log=%s\n", proto.MarshalTextString(req))
 	return &collogspb.ExportLogsServiceResponse{}, nil
 }
 
 func (x *OtelMetrics) Export(ctx context.Context, req *colmetricspb.ExportMetricsServiceRequest) (*colmetricspb.ExportMetricsServiceResponse, error) {
+	if x.finished {
+		return &colmetricspb.ExportMetricsServiceResponse{}, nil
+	}
 	//log.Printf("metric=%s\n", proto.MarshalTextString(req))
+	for _, rm := range req.ResourceMetrics {
+		log.Printf("resource=%s\n", proto.MarshalTextString(rm.Resource))
+		for _, sm := range rm.ScopeMetrics {
+			log.Printf("scope=%s\n", proto.MarshalTextString(sm.Scope))
+			for _, m := range sm.Metrics {
+				if sum := m.GetSum(); sum != nil {
+					for _, dp := range sum.DataPoints {
+						dp.TimeUnixNano = 0
+					}
+				}
+				if want, ok := x.verify.want[m.Name]; ok {
+					if diff := cmp.Diff(m, want, protocmp.Transform()); diff != "" {
+						log.Printf("mismatched metric: %v\n", diff)
+					} else {
+						delete(x.verify.want, m.Name)
+					}
+				}
+			}
+		}
+	}
+	if len(x.verify.want) == 0 {
+		x.finished = true
+		close(x.verify.done)
+	}
 	return &colmetricspb.ExportMetricsServiceResponse{}, nil
+}
+
+func (x *Otel) Wait() Step {
+	return &x.verify
+}
+
+func (v *verify) Run(p *Params) error {
+	d := 30 * time.Second
+	select {
+	case <-v.done:
+		return nil
+	case <-time.After(d):
+		return fmt.Errorf("timed out waiting for all metrics to match")
+	}
+}
+func (v *verify) Cleanup() {
 }
 
 var _ Step = &Otel{}
@@ -61,6 +118,13 @@ func (x *Otel) Run(p *Params) error {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", x.Port))
 	if err != nil {
 		return err
+	}
+	x.verify.done = make(chan struct{})
+	x.verify.want = make(map[string]*metricspb.Metric)
+	for _, m := range x.Metrics {
+		mpb := &metricspb.Metric{}
+		p.LoadTestProto(m, mpb)
+		x.verify.want[mpb.Name] = mpb
 	}
 	go func() {
 		_ = x.grpc.Serve(lis)
