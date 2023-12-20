c4a6795dc0ec7dfd297794a32b5604042f5a7e6e
Yuan Yan <yuanyan@google.com>
2023-12-20 06:44:21 +0000

    test a change

--- extensions/stackdriver/metric/record.cc
+++ extensions/stackdriver/metric/record.cc
@@ -110,8 +110,7 @@ TagKeyValueList getOutboundTagMap(const ::Wasm::Common::FlatNode& local_node_inf
        unknownIfEmpty(flatbuffers::GetString(local_node_info.namespace_()))},
       {sourceCanonicalRevisionKey(), unknownIfEmpty(getLocalCanonicalRev(local_node_info))},
       {proxyVersionKey(), unknownIfEmpty(flatbuffers::GetString(local_node_info.istio_version()))},
-      {responseFlagKey(), unknownIfEmpty(request_info.response_flag)},
-      };
+      {responseFlagKey(), unknownIfEmpty(request_info.response_flag)}};
   return outboundMap;
 }
 
@@ -151,8 +150,7 @@ TagKeyValueList getInboundTagMap(const ::Wasm::Common::FlatNode& local_node_info
        unknownIfEmpty(flatbuffers::GetString(peer_node_info.namespace_()))},
       {sourceCanonicalRevisionKey(), unknownIfEmpty(getPeerCanonicalRev(peer_node_info))},
       {proxyVersionKey(), unknownIfEmpty(flatbuffers::GetString(local_node_info.istio_version()))},
-      {responseFlagKey(), unknownIfEmpty(request_info.response_flag)},
-      };
+      {responseFlagKey(), unknownIfEmpty(request_info.response_flag)}};
   return inboundMap;
 }
 
