ea110c6ce4a0208af2f5a6e11497cc12269be6f0
whitneygriffith <whitney.griffith16@gmail.com>
2024-04-16 15:37:53 +0000

    add comments
    Signed-off-by: whitneygriffith <whitney.griffith16@gmail.com>

--- extensions/common/context.h
+++ extensions/common/context.h
@@ -91,6 +91,7 @@ std::string_view ProtocolString(Protocol protocol);
 
 // RequestInfo represents the information collected from filter stream
 // callbacks. This is used to fill metrics and logs.
+// TODO: do we want to include waypoint specific variables and maintain source and dest as the workloads?
 struct RequestInfo {
   // Start timestamp in nanoseconds.
   int64_t start_time;
