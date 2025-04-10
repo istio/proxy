f5e351550cc92e52647ba986983a57ddf69e326b
zirain <zirain2009@gmail.com>
2025-04-10 19:53:36 +0800

    chore: find failed on mac

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -67,13 +67,13 @@ lint-licenses:
 lint-all: lint-dockerfiles lint-scripts lint-yaml lint-helm lint-copyright-banner lint-go lint-python lint-markdown lint-sass lint-typescript lint-licenses
 
 tidy-go:
-	@find -name go.mod -execdir go mod tidy \;
+	@${FINDFILES} -name go.mod -execdir go mod tidy \;
 
 mod-download-go:
 	@-GOFLAGS="-mod=readonly" find -name go.mod -execdir go mod download \;
 # go mod tidy is needed with Golang 1.16+ as go mod download affects go.sum
 # https://github.com/golang/go/issues/43994
-	@find -name go.mod -execdir go mod tidy \;
+	@${FINDFILES} -name go.mod -execdir go mod tidy \;
 
 format-go: tidy-go
 	@${FINDFILES} -name '*.go' \( ! \( -name '*.gen.go' -o -name '*.pb.go' \) \) -print0 | ${XARGS} common/scripts/format_go.sh
